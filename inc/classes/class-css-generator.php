702e05ed4c0851594c73d422b0ccc57d15a0d959
vannha <phongnhatk21@gmail.com>
2019-11-11 14:03:44 +0700

    commit -

--- inc/classes/class-css-generator.php
+++ inc/classes/class-css-generator.php
@@ -47,7 +47,7 @@ class OverCome_CSS_Generator
     function __construct()
     {
         $this->opt_name = theclick_get_theme_opt_name();
-var_dump($this->opt_name); die('abc');
+ 
         if ( empty( $this->opt_name ) )
         {
             return;
@@ -72,7 +72,7 @@ var_dump($this->opt_name); die('abc');
             return;
         }
         $this->dev_mode = theclick_get_theme_opt('dev_mode', false);
-        
+
         if ( $this->dev_mode )
         {
             $this->generate_file();
