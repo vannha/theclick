212c5a50059b812902f2e58a60b40cc3f9f1c96c
vannha <phongnhatk21@gmail.com>
2019-11-13 10:53:58 +0700

    commit -

--- vc_templates/ef5_user_block.php
+++ vc_templates/ef5_user_block.php
@@ -25,7 +25,7 @@ $wrapper_class = array('ef5-user-block', $el_class);
         	echo 'abbbb';
             //echo fsUser()->get_template_file__('logout', array('atts' => $config), '', 'flex-login');
 
-            return;
+            //return;
         }
         /*wp_enqueue_style('fs-user-form.css', fsUser()->plugin_url . 'assets/css/fs-user-form.css', array(), '', 'all');
         wp_enqueue_script('jquery.validate.js', fsUser()->plugin_url . 'assets/vendor/jquery.validate.js', array(), '', true);
