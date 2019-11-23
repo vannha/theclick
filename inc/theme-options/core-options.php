531dfc1215d74ef28076d8049c7951f74e17392e
vannha <phongnhatk21@gmail.com>
2019-11-23 14:57:51 +0700

    commit -

--- inc/theme-options/core-options.php
+++ inc/theme-options/core-options.php
@@ -279,16 +279,6 @@ if(!function_exists('theclick_header_opts')){
                 'title' => esc_html__('Link colors', 'theclick'),
             ),
             array(
-                'title'    => esc_html__('After Icon of Menu Parent', 'theclick'),
-                'id'       => 'menu_parent_icon_after',
-                'type'     => 'button_set',
-                'options'  => array(
-                    '1'  => esc_html__('Show','theclick'),
-                    '0'  => esc_html__('Hide','theclick')
-                ),
-                'default'  => '0'
-            ),
-            array(
                 'id'       => 'header_border',
                 'type'     => 'border',
                 'all'      => false,
