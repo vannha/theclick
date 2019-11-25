a684806c3923585becbde5bf6a7ec9e32872b3d9
vannha <phongnhatk21@gmail.com>
2019-11-25 14:00:10 +0700

    commit -

--- inc/functions/header-profile.php
+++ inc/functions/header-profile.php
@@ -6,9 +6,14 @@
 if(!function_exists('theclick_header_signin_signup')){
 	function theclick_header_signin_signup($args = []){
 		if(!class_exists('FlexUser')) return;
-		$header_user = theclick_get_opts('header_user','0');
-		var_dump($header_user);
+		$login_register = theclick_get_opts('login_register','0');
+		if($login_register == '0') return;
+		$login_regis_type     = theclick_get_opts('login_regis_type', 'both');
+		$login_regis_num_link = theclick_get_opts('login_regis_num_link', '2');
+		$login_regis_active   = theclick_get_opts('login_regis_active', 'login');  
+		$login_description    = theclick_get_opts('login_description','');
+		$register_description = theclick_get_opts('register_description', '');
 		$authid = rand(4,9999);
-		echo do_shortcode( '[fu_auth el_id="'.$authid.'"]' );
+		echo do_shortcode( '[fu_auth el_id="'.$authid. '" type="both" num_link="1" active="register" login_description="desc1" register_description="desc2"]' );
 	}
 }
