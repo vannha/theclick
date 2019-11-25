6a856e84578cee1c0896dfd59d4f120cfcc4963e
vannha <phongnhatk21@gmail.com>
2019-11-25 13:49:42 +0700

    commit -

--- inc/functions/header-profile.php
+++ inc/functions/header-profile.php
@@ -6,7 +6,8 @@
 if(!function_exists('theclick_header_signin_signup')){
 	function theclick_header_signin_signup($args = []){
 		if(!class_exists('FlexUser')) return;
-		
+		$header_user = theclick_get_opts('header_user','0');
+		var_dump($header_user);
 		$authid = rand(4,9999);
 		echo do_shortcode( '[fu_auth el_id="'.$authid.'"]' );
 	}
