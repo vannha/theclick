9ef78e00961a33d1286723a3b1566dabf956d5fc
vannha <phongnhatk21@gmail.com>
2019-11-27 16:23:11 +0700

    commit -

--- template-parts/header/layout-1.php
+++ template-parts/header/layout-1.php
@@ -20,12 +20,12 @@
                             <div class="header-attr-wrap nav-extra">
                                 <?php
                                 get_template_part('template-parts/header/header-social');
-                                theclick_header_signin_signup();
+                                theclick_header_signin_signup(['class' => 'd-none d-sm-block']);
                                 theclick_header_search(['type' => 'popup']);
-                                theclick_header_wishlist();
-                                theclick_header_compare();
-                                theclick_header_cart();
-                                theclick_header_popup_nav_icon();
+                                theclick_header_wishlist(['class' => 'd-none d-sm-block']);
+                                theclick_header_compare(['class' => 'd-none d-sm-block']);
+                                theclick_header_cart(['class' => 'd-none d-sm-block']);
+                                theclick_header_popup_nav_icon(['class' => 'd-none d-sm-block']);
                                 theclick_header_mobile_menu_icon();
                                 ?>
                             </div>
