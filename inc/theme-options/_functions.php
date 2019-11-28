13e6749f4e747c02db3331e696f159fbf214fea7
vannha <phongnhatk21@gmail.com>
2019-11-28 14:02:49 +0700

    commit -

--- inc/theme-options/_functions.php
+++ inc/theme-options/_functions.php
@@ -46,7 +46,7 @@ function theclick_list_post_thumbnail($post_type = 'post', $default = false){
         $layouts['-1'] = get_template_directory_uri() . '/assets/images/default.png';
         $layouts['none'] = get_template_directory_uri() . '/assets/images/none.png';
     }
-    $posts = get_posts(array('post_type' => $post_type,'posts_per_page' => '-1'));
+    $posts = get_posts(array('post_type' => $post_type,'posts_per_page' => '-1','orderby' => 'date', 'order' => 'DESC'));
     foreach($posts as $post){
         $layouts[$post->post_name] = [
             'alt' => get_the_title($post->ID),
