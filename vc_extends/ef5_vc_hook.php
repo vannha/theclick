176fcdec540b29e81c76bd14045e8f1323c80adf
vannha <phongnhatk21@gmail.com>
2019-11-28 16:36:04 +0700

    commit -

--- vc_extends/ef5_vc_hook.php
+++ vc_extends/ef5_vc_hook.php
@@ -34,8 +34,8 @@ function unbreak_vc_shortcode_output($html = '', $sc_obj = '', $atts = [])
             // Stretch row style 2
             if(!isset($full_width)){
                 $container_class[] = 'container-wide';
-                $modify['first-child'] .= '<div class="'.implode(' ', $container_class).'"><div class="row">';
-                $modify['last-child']  .= '</div></div>';
+                $modify['before'] .= '<div class="'.implode(' ', $container_class).'">';
+                $modify['after']  .= '</div>';
             }
             break;
         default:
