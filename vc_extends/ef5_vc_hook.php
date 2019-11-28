e1379b298ec1d746b6ea706e10d009e4fbd37af8
vannha <phongnhatk21@gmail.com>
2019-11-28 16:13:46 +0700

    commit -

--- vc_extends/ef5_vc_hook.php
+++ vc_extends/ef5_vc_hook.php
@@ -1,3 +1,9 @@
 <?php
-// silent is golden
+add_action('vc_after_init', 'theclick_vc_row');
+function theclick_vc_row() {   
+    $param = WPBMap::getParam('vc_row', 'full_width');
+    $param['value'][esc_html__('Stretch row and content 2','theclick')] = 'stretch_row_content2';
+    $param['std'] = 'stretch_row_content2';
+    vc_update_shortcode_param('vc_row', $param);
+}
    
