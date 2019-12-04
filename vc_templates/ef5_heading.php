912229bb5a98f023cb438f34c56b1d384c12f4b6
vannha <phongnhatk21@gmail.com>
2019-12-04 09:08:50 +0700

    commit -

--- vc_templates/ef5_heading.php
+++ vc_templates/ef5_heading.php
@@ -15,7 +15,7 @@ extract( $atts );
 	<?php 
 		switch ($layout_template) {
 			case '2':
-				$this->ef5_heading_main_heading($atts,['class' => 'text-24 ef5-heading']);
+				$this->ef5_heading_main_heading($atts,['class' => 'text-24 ef5-heading lh-32']);
 				$this->ef5_heading_sub_heading($atts, ['class' => 'text-22 text-white']);
 				$this->ef5_heading_desccription($atts);
 				$this->ef5_heading_button($atts,['class' => 'ef5-btn accent outline ef5-btn-md']);
