961e25a16d737b746e689993687ae2f05106d205
vannha <phongnhatk21@gmail.com>
2019-12-10 15:04:26 +0700

    commit -

--- inc/functions/header-social.php
+++ inc/functions/header-social.php
@@ -94,5 +94,5 @@ function theclick_header_social_counter($args=[]){
     ]);
     $header_social_counter = theclick_get_opts('header_social_counter', '0');
     if ($header_social_counter === '0') return;
-    echo do_shortcode( '[aps-get-count social_media="facebook"]', false );
+    //echo do_shortcode( '[aps-get-count social_media="facebook"]', false );
 }
