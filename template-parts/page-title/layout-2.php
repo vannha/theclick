d07871316fc4f7894b0985015f131503b9bddcf4
vannha <phongnhatk21@gmail.com>
2019-12-11 11:15:40 +0700

    commit -

--- template-parts/page-title/layout-2.php
+++ template-parts/page-title/layout-2.php
@@ -5,49 +5,49 @@ $ptitle_layout = theclick_get_opts('ptitle_layout', '1');
 
 $pt_cls = array(
     'ef5-pagetitle',
-    'ptitle-layout-'.$ptitle_layout
+    'ptitle-layout-' . $ptitle_layout
 );
 $title_css_class = ['col-12'];
 
 
-if(!$show_breadcrumb) {
+if (!$show_breadcrumb) {
     $pt_cls[] = 'no-breadcrumb';
-} 
-if($show_breadcrumb && (!is_home() || !is_front_page())) {
+}
+if ($show_breadcrumb && (!is_home() || !is_front_page())) {
     $title_css_class[] = 'col-lg-6';
 }
 
 ob_start();
-    if ( $titles['title'] )
-    {
-        printf( '<div class="page-title h1">%s</div>', $titles['title']);
-    }
+if ($titles['title']) {
+    printf('<div class="page-title h1">%s</div>', $titles['title']);
+}
 
-    if ( $titles['desc'] )
-    {
-        printf( '<div class="page-desc">%s</div>', $titles['desc']);
-    }
+if ($titles['desc']) {
+    printf('<div class="page-desc">%s</div>', $titles['desc']);
+}
 
 $titles_html = ob_get_clean();
 
-if ( ! $titles_html )
-{
+if (!$titles_html) {
     return;
 }
 ?>
 <div class="ef5-pagetitle-wrap">
-    <div class="<?php echo implode(' ', $pt_cls);?>">
+    <div class="<?php echo implode(' ', $pt_cls); ?>">
         <?php theclick_ptitle_parallax_image(); ?>
-        <?php theclick_ptitle_logo(); ?>
-        <div class="<?php theclick_ptitle_inner_class();?>">
+
+        <div class="<?php theclick_ptitle_inner_class(); ?>">
             <div class="row align-items-center">
-                <div class="<?php echo trim(implode(' ', $title_css_class));?>">
-                    <?php printf( '%s', $titles_html); ?>
+                <div class="col-12">
+                    <?php theclick_ptitle_logo(); ?>
                 </div>
-                <?php if($show_breadcrumb && (!is_home() || !is_front_page())) { ?>
-                <div class="ef5-breadcrumb">
-                    <?php theclick_breadcrumb(['class'=>'h5']); ?>
+                <div class="<?php echo trim(implode(' ', $title_css_class)); ?>">
+                    <?php printf('%s', $titles_html); ?>
                 </div>
+                <?php if ($show_breadcrumb && (!is_home() || !is_front_page())) { ?>
+                    <div class="ef5-breadcrumb">
+                        <?php theclick_breadcrumb(['class' => 'h5']); ?>
+                    </div>
                 <?php } ?>
             </div>
         </div>
