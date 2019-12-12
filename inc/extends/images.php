226d838ad9d885697aab20c5cbe152e15feb882a
vannha <phongnhatk21@gmail.com>
2019-12-12 10:22:12 +0700

    commit -

--- inc/extends/images.php
+++ inc/extends/images.php
@@ -10,7 +10,7 @@ function theclick_get_image_sizes() {
     global $_wp_additional_image_sizes;
     $sizes = array();
     foreach ( get_intermediate_image_sizes() as $_size ) {
-        if ( in_array( $_size, array('thumbnail', 'medium', 'medium_large', 'large') ) ) {
+        if ( in_array( $_size, array('thumbnail', 'medium', 'medium_large', 'large','post-thumbnail') ) ) {
             $sizes[ $_size ]['width']  = get_option( "{$_size}_size_w" );
             $sizes[ $_size ]['height'] = get_option( "{$_size}_size_h" );
             $sizes[ $_size ]['crop']   = (bool) get_option( "{$_size}_crop" );
