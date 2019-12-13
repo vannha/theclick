495002a605428ff168393793fb570cd7dda7eca2
vannha <phongnhatk21@gmail.com>
2019-12-13 09:02:19 +0700

    commit -

--- inc/functions/template-functions.php
+++ inc/functions/template-functions.php
@@ -31,17 +31,22 @@ if(!function_exists('theclick_post_extra_link')){
         $post_extra_link        = theclick_get_theme_opt('post_extra_link','');
         $post_number_extra_link = theclick_get_theme_opt('post_number_extra_link', '2');
         $post_extra_post_id     = theclick_get_theme_opt('post_extra_post_id', '');
+        $pids = explode(',',$post_extra_post_id);
         if(empty($post_extra_link)) return;
-        $recent_posts = wp_get_recent_posts(['numberposts'=> $post_number_extra_link]);
-         
+        if($post_extra_link == 'recent')
+            $extra_posts = wp_get_recent_posts(['numberposts'=> $post_number_extra_link]);
+        else
+            $extra_posts = get_posts(['include' => $pids]);
+
+            var_dump($extra_posts); die;
         ?>
         <div class="ef5-post-extra-link">
         <ul>
-        <?php foreach ($recent_posts as $recent) {
+        <?php foreach ($extra_posts as $extra) {
             printf(
                 '<li><a href="%1$s">%2$s</a></li>',
-                esc_url(get_permalink($recent['ID'])),
-                apply_filters('the_title', $recent['post_title'], $recent['ID'])
+                esc_url(get_permalink($extra['ID'])),
+                apply_filters('the_title', $extra['post_title'], $extra['ID'])
             );
         }
 	    ?>
