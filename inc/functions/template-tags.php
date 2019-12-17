1b870d322829540f40a1bded63758e9558d3f5be
vannha <phongnhatk21@gmail.com>
2019-12-17 10:03:45 +0700

    commit -

--- inc/functions/template-tags.php
+++ inc/functions/template-tags.php
@@ -140,7 +140,7 @@ if ( ! function_exists( 'theclick_posted_in' ) ) :
     function theclick_posted_in($args = []){
         $args = wp_parse_args($args, [
             'class'           => '',
-            'hint'            => esc_html__( 'Posted in', 'theclick' ),
+            'hint'            => '', //esc_html__( 'Posted in', 'theclick' ),
             'icon'            => '', //'fa fa-folder',
             'before'          => '',
             'after'           => '',
