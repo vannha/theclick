9b2b7df8282601a2a80d17f989237d3469f40c37
vannha <phongnhatk21@gmail.com>
2019-12-17 10:43:59 +0700

    commit -

--- inc/functions/template-tags.php
+++ inc/functions/template-tags.php
@@ -239,7 +239,7 @@ if ( ! function_exists( 'theclick_comments_popup_link' ) ) {
     {
         $args = wp_parse_args($args, [
             'class'     => '',
-            'hint'      => 'a',
+            'hint'      => '',
             'before'    => '',
             'after'     => '',
             'icon'      => '', //'fa fa-comment-alt',
