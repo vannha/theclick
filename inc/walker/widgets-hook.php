a82fb66d9090256d08ec716a1faef03f981e8c65
vannha <phongnhatk21@gmail.com>
2019-12-18 09:25:27 +0700

    commit -

--- inc/walker/widgets-hook.php
+++ inc/walker/widgets-hook.php
@@ -7,13 +7,13 @@
  * Widget Categories
  * Custom HTML output
 */
-if(!function_exists('theclick_widget_categories_args')){
+/*if(!function_exists('theclick_widget_categories_args')){
     add_filter('widget_categories_args', 'theclick_widget_categories_args');
     function theclick_widget_categories_args($cat_args){
         $cat_args['walker'] = new TheClick_Categories_Walker;
         return $cat_args; 
     }
-}
+}*/
 
 /** 
  * Custom Widget Archive 
