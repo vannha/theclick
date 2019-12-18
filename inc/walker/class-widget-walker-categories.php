eae0754f1e9828d55977987f0f09e07ee9c3f877
vannha <phongnhatk21@gmail.com>
2019-12-18 10:24:56 +0700

    commit -

--- inc/walker/class-widget-walker-categories.php
+++ inc/walker/class-widget-walker-categories.php
@@ -65,8 +65,8 @@ class TheClick_Categories_Walker extends Walker_Category {
             if(class_exists('Taxonomy_Images_Supported')){
                 //$obj = get_queried_object();
                 //$image_url = apply_filters( 'taxonomy-images-list-the-terms', '' );
-                $terms = apply_filters( 'taxonomy-images-get-terms', '' );
-                var_dump($terms);
+                //$terms = apply_filters( 'taxonomy-images-get-terms', '' );
+                var_dump($category);
             }
             $link .= '<span class="title">'.$cat_name.'</span>';
             if ( ! empty( $args['show_count'] ) ) {
