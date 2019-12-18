f0b44b40b4279bc79858ded7eb8d680927b1c033
vannha <phongnhatk21@gmail.com>
2019-12-18 09:22:38 +0700

    commit -

--- inc/widgets/widgets-hook.php
+++ inc/widgets/widgets-hook.php
@@ -8,11 +8,11 @@
 */
 if(!function_exists('theclick_widget_categories_args')){
     add_filter('widget_categories_args', 'theclick_widget_categories_args');
-    function theclick_widget_categories_args($cat_args){ die('abx');
-        $cat_args['walker'] = new TheClick_Categories_Walker;
+    function theclick_widget_categories_args($cat_args){  
+        //$cat_args['walker'] = new TheClick_Categories_Walker;
         $cat_args['orderby'] = 'count';
         $cat_args['order'] = 'DESC';
-        return ''; //$cat_args; 
+        return $cat_args; 
     }
 }
 
