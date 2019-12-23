fb2726687c082f0a77e8e550554f690e22a0cb24
vannha <phongnhatk21@gmail.com>
2019-12-23 12:01:21 +0700

    commit -

--- inc/functions/_instagram.php
+++ inc/functions/_instagram.php
@@ -116,9 +116,7 @@ if (!function_exists('theclick_instagram_custom_layout')) {
 // Output HTML 
 if (!function_exists('theclick_instagram_html_output')) {
     add_filter('ef5systems_instagram_output_html', 'theclick_instagram_html_output', 10, 1);
-    function theclick_instagram_html_output($args = [])
-    {
-        //extract($args);
+    function theclick_instagram_html_output($args = []){
         $args = wp_parse_args($args, [
             'layout_mode'   => 'default',
             'span'          => '4',
