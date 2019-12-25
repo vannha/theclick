03b7a4b1b4abe637e6cc2cf145ae4efa1e9aff43
vannha <phongnhatk21@gmail.com>
2019-12-25 10:52:39 +0700

    commit -

--- single.php
+++ single.php
@@ -25,12 +25,16 @@ get_header();
                 while (have_posts()) :
                     the_post();
                     get_template_part('template-parts/single/content', get_post_format());
-                    theclick_comment();
                 endwhile; // End of the loop.
                 ?>
             </div>
         </div>
         <?php theclick_sidebar(); ?>
+    </div>
+    <div class="row">
+        <div class="col-12 col-xl-8 offset-xl-2">
+        <?php theclick_comment(); ?>
+        </div>
     </div>
     <?php theclick_post_related(); ?>
 </div>
