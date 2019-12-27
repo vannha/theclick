320cb768f3d56f98c91cc57df83c6cf643c9e8d0
vannha <phongnhatk21@gmail.com>
2019-12-27 14:52:16 +0700

    commit -

--- vc_extends/ef5_vc_hook.php
+++ vc_extends/ef5_vc_hook.php
@@ -1,2 +1,9 @@
 <?php
 // silent is the golden
+add_filter('ef5systems_vc_column_width','testfix');
+function testfix(){
+	return [
+		esc_html__( '43%', 'theclick' ) => '29/5082',
+		esc_html__( '57%', 'theclick' ) => '70/4918'
+	]
+}
