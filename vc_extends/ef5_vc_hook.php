b7c8d8a2824d036611dac4727556c3650d3723b9
vannha <phongnhatk21@gmail.com>
2019-12-27 15:16:43 +0700

    commit -

--- vc_extends/ef5_vc_hook.php
+++ vc_extends/ef5_vc_hook.php
@@ -1,7 +1,7 @@
 <?php
 // silent is the golden
-add_filter('ef5systems_vc_column_width','testfix');
-function testfix(){
+add_filter('ef5systems_vc_column_width','theclick_update_vc_grid_column_width');
+function theclick_update_vc_grid_column_width(){
 	return [
 		esc_html__( '43%', 'theclick' ) => '29/509',
 		esc_html__( '57%', 'theclick' ) => '70/491'
