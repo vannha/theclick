8bc7936efbc5672acb865afad57d09c153c7ddfc
vannha <phongnhatk21@gmail.com>
2020-01-02 11:32:37 +0700

    commit -

--- vc_templates/ef5_heading.php
+++ vc_templates/ef5_heading.php
@@ -10,24 +10,24 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
 extract( $atts );
-$text_clss = ['ef5-heading'];
-if(!empty($heading_text_sm)) $text_clss[] = 'text-'.$heading_text_sm;
-if(!empty($heading_text_md)) $text_clss[] = 'text-md-'.$heading_text_md;
-if(!empty($heading_text_lg)) $text_clss[] = 'text-lg-'.$heading_text_lg;
-if(!empty($heading_text_xl)) $text_clss[] = 'text-xl-'.$heading_text_xl;
-$text_cls = implode(' ', $text_clss);
+$heading_text_clss = ['ef5-heading'];
+if(!empty($heading_text_sm)) $heading_text_clss[] = 'text-'.$heading_text_sm;
+if(!empty($heading_text_md)) $heading_text_clss[] = 'text-md-'.$heading_text_md;
+if(!empty($heading_text_lg)) $heading_text_clss[] = 'text-lg-'.$heading_text_lg;
+if(!empty($heading_text_xl)) $heading_text_clss[] = 'text-xl-'.$heading_text_xl;
+$heading_text_cls = implode(' ', $heading_text_clss);
 ?>
 <div class="<?php $this->theclick_heading_wrap_css_class($atts); ?>">
 	<?php 
 		switch ($layout_template) {
 			case '2':
-				$this->ef5_heading_main_heading($atts,['class' => $text_cls]);
+				$this->ef5_heading_main_heading($atts,['class' => $heading_text_cls]);
 				$this->ef5_heading_sub_heading($atts, ['class' => 'text-22 text-white']);
 				$this->ef5_heading_desccription($atts);
 				$this->ef5_heading_button($atts,['class' => 'ef5-btn accent outline ef5-btn-md']);
 				break;
 			default:
-				$this->ef5_heading_main_heading($atts,['class' => $text_cls]);
+				$this->ef5_heading_main_heading($atts,['class' => $heading_text_cls]);
 				$this->ef5_heading_sub_heading($atts, ['class' => 'text-22']);
 				$this->ef5_heading_desccription($atts);
 				$this->ef5_heading_button($atts,['class' => 'ef5-btn accent fill ef5-btn-md']);
