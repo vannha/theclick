b4c7b2fe73dba69dc4961b9edf4865330d63077d
vannha <phongnhatk21@gmail.com>
2020-01-03 16:14:28 +0700

    commit -

--- vc_elements/ef5_posts.php
+++ vc_elements/ef5_posts.php
@@ -242,9 +242,9 @@ class WPBakeryShortCode_ef5_posts extends WPBakeryShortCode{
         <div class="<?php echo trim(implode(' ', $css_class));?>">
             <?php 
             theclick_post_meta_category(['class' => 'meta-category justify-content-center']);
-            the_title( '<div class="ef5-heading h2"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">','</a></div>' );
+            the_title( '<div class="ef5-heading h2 text-center"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">','</a></div>' );
             theclick_post_media(['thumbnail_size' => 'large', 'default_thumb' => true,'after' => '','img_class' => '']);
-            theclick_post_excerpt(['show_excerpt' => '1', 'length' => '62', 'more' => '...' ]);
+            theclick_post_excerpt(['show_excerpt' => '1', 'length' => '62', 'more' => '...','class' => 'text-center' ]);
             theclick_post_meta(['class' => 'justify-content-center','show_author' => '1','show_date' => '1','show_cmt' => '1']);
             ?> 
         </div>
