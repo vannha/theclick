5d2af7b14b49405b6a2398024442f311be965f43
vannha <phongnhatk21@gmail.com>
2020-01-15 10:41:12 +0700

    commit -

--- vc_templates/ef5_banner.php
+++ vc_templates/ef5_banner.php
@@ -3,20 +3,20 @@ $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
 ?>
 <div class="<?php $this->theclick_banner_wrap_css_class($atts); ?>">
     <?php 
-        switch ($atts['banner_style']) {
-            case '1':
-                $this->ef5_banner_main_banner($atts,['class' => '']);
-                $this->ef5_banner_main_title($atts,['class' => '']);
-                $this->ef5_banner_sub_title($atts,['class' => '']);
-                $this->ef5_banner_button($atts,['class' => '']);
-                break;
-            default:
-                $this->ef5_banner_main_banner($atts,['class' => '']);
-                $this->ef5_banner_main_title($atts,['class' => '']);
-                $this->ef5_banner_sub_title($atts,['class' => '']);
-                $this->ef5_banner_button($atts,['class' => '']);
-                break;
-        }
+    switch ($atts['banner_style']) {
+        case '1':
+            $this->ef5_banner_main_banner($atts,['class' => '']);
+            $this->ef5_banner_main_title($atts,['class' => '']);
+            $this->ef5_banner_sub_title($atts,['class' => '']);
+            $this->ef5_banner_button($atts,['class' => '']);
+            break;
+        default:
+            $this->ef5_banner_main_banner($atts,['class' => '']);
+            $this->ef5_banner_main_title($atts,['class' => '']);
+            $this->ef5_banner_sub_title($atts,['class' => '']);
+            $this->ef5_banner_button($atts,['class' => '']);
+            break;
+    }
     ?>
 </div>
   
