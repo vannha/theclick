45a0f7b47bb41dc47dd4d93161ab174fde439839
vannha <phongnhatk21@gmail.com>
2020-01-31 08:38:37 +0700

    commit -

--- vc_templates/ef5_product_grid.php
+++ vc_templates/ef5_product_grid.php
@@ -13,6 +13,7 @@
     }*/
     if(is_front_page()) {
         $paged = (get_query_var('page')) ? get_query_var('page') : 1;
+        var_dump($paged);
     }else {
         $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
     }
