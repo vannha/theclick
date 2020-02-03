f54d7f6a63e2bb1279e8a37ef9a1d6c2192f5c22
vannha <phongnhatk21@gmail.com>
2020-02-03 11:22:26 +0700

    commit -

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -684,6 +684,8 @@
             vertical: false,
             slidesToShow: 1,
             focusOnSelect: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
             infinite: false,
         });
         
