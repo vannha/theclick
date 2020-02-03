d380bbb032a395d65e492de6d3e10137133585ba
vannha <phongnhatk21@gmail.com>
2020-02-03 14:02:48 +0700

    commit -

--- inc/woo/_hook_exts.php
+++ inc/woo/_hook_exts.php
@@ -6,6 +6,35 @@
 */
 
 /**
+ * Custom Woo Smart Compare
+ * 
+ * Change Compare button position
+ *
+*/
+$wc_compare_to_attrs = apply_filters('theclick_wc_compare_to_attrs', '1');
+if(class_exists('WPcleverWooscp')){
+    // Loop
+    if($wc_compare_to_attrs === '1'){
+        add_filter( 'filter_wooscp_button_archive', function() { return '0'; } );
+        add_action('theclick_woocommerce_loop_product_add_to_cart', 'theclick_wooscp_icon', 11);
+        function theclick_wooscp_icon(){
+            global $product;
+            $wooscp_text = apply_filters('wooscp_button_text', get_option( '_wooscp_button_text', esc_html__( 'Compare', 'theclick' ) ));
+            echo '<div class="woosmart-icon compare-icon hint--top-'.theclick_align2().'" data-selected="'.esc_html__('Open Compared List','theclick').'" data-hint="'.esc_html($wooscp_text).'">'.do_shortcode('[wooscp id="'.$product->get_id().'" type="link"]').'</div>';
+        }
+    }
+
+    // Single 
+    add_filter( 'filter_wooscp_button_single', function() { return '0'; } ); 
+    add_action('woocommerce_after_add_to_cart_button', 'theclick_wooscp_button', 10);
+    function theclick_wooscp_button(){
+        global $product;
+        $wooscp_text = apply_filters('wooscp_button_text', get_option( '_wooscp_button_text', esc_html__( 'Compare', 'theclick' ) ));
+        echo '<div class="wc-single-btn woosmart-btn compare-btn hint--bounce hint--bottom" data-selected="'.esc_html__('Open Compared List','theclick').'" data-hint="'.esc_html($wooscp_text).'">'.do_shortcode('[wooscp id="'.$product->get_id().'"]').'</div>';
+    }
+}
+
+/**
  * Custom Woo Smart Quick View
  *
  * Change Quick View button position
@@ -18,7 +47,7 @@ if(class_exists('WPcleverWoosq') && $wc_quickview_to_attrs === '1'){
         global $product;
         $woosq_text = apply_filters('woosq_button_text', get_option( 'woosq_button_text', esc_html__( 'Quick view', 'theclick' ) ));
         echo '<div class="woosmart-icon quickview-icon hint--top-'.theclick_align2().'" data-hint="'.esc_html($woosq_text).'">'.do_shortcode('[woosq id="'.$product->get_id().'" type="link"]').'</div>';
-    },11);
+    },12);
 }
 /**
  * Custom Woo Smart Add to Wishlist
@@ -36,7 +65,7 @@ if(class_exists('WPcleverWoosw')){
             $woosw_text  = apply_filters( 'woosw_button_text', get_option( 'woosw_button_text', esc_html__( 'Add to Wishlist', 'theclick' ) ) );
 	        $woosw_text_added  = apply_filters( 'woosw_button_text_added', get_option( 'woosw_button_text_added', esc_html__( 'Browse Wishlist', 'theclick' ) ) );
 	        echo '<div class="woosmart-icon wishlist-icon hint--top-'.theclick_align2().'" data-selected="'.esc_html($woosw_text_added).'" data-hint="'.esc_html($woosw_text).'">'.do_shortcode('[woosw id="'.$product->get_id().'" type="link"]').'</div>';
-	    },12);
+	    },13);
 	}
     // Single
     add_filter( 'woosw_button_position_single', function() {
@@ -50,31 +79,3 @@ if(class_exists('WPcleverWoosw')){
     },12);
 }
 
-/**
- * Custom Woo Smart Compare
- * 
- * Change Compare button position
- *
-*/
-$wc_compare_to_attrs = apply_filters('theclick_wc_compare_to_attrs', '1');
-if(class_exists('WPcleverWooscp')){
-    // Loop
-    if($wc_compare_to_attrs === '1'){
-        add_filter( 'filter_wooscp_button_archive', function() { return '0'; } );
-        add_action('theclick_woocommerce_loop_product_add_to_cart', 'theclick_wooscp_icon', 13);
-        function theclick_wooscp_icon(){
-            global $product;
-            $wooscp_text = apply_filters('wooscp_button_text', get_option( '_wooscp_button_text', esc_html__( 'Compare', 'theclick' ) ));
-            echo '<div class="woosmart-icon compare-icon hint--top-'.theclick_align2().'" data-selected="'.esc_html__('Open Compared List','theclick').'" data-hint="'.esc_html($wooscp_text).'">'.do_shortcode('[wooscp id="'.$product->get_id().'" type="link"]').'</div>';
-        }
-    }
-
-    // Single 
-    add_filter( 'filter_wooscp_button_single', function() { return '0'; } ); 
-    add_action('woocommerce_after_add_to_cart_button', 'theclick_wooscp_button', 10);
-    function theclick_wooscp_button(){
-        global $product;
-        $wooscp_text = apply_filters('wooscp_button_text', get_option( '_wooscp_button_text', esc_html__( 'Compare', 'theclick' ) ));
-        echo '<div class="wc-single-btn woosmart-btn compare-btn hint--bounce hint--bottom" data-selected="'.esc_html__('Open Compared List','theclick').'" data-hint="'.esc_html($wooscp_text).'">'.do_shortcode('[wooscp id="'.$product->get_id().'"]').'</div>';
-    }
-}
