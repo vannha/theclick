dd527164baa636ef8dd665df3dcc1b1efe1663bd
vannha <phongnhatk21@gmail.com>
2020-02-03 15:03:00 +0700

    commit -

--- inc/woo/_hook_loop_products.php
+++ inc/woo/_hook_loop_products.php
@@ -9,8 +9,8 @@ remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_l
 
 remove_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10);
 
-remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5);
-remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10);
+//remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5);
+//remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10);
 
 remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
 remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
