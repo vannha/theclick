8bf1696e12e81b810621cdb310d56a0ddee48340
vannha <phongnhatk21@gmail.com>
2020-02-04 08:49:18 +0700

    commit -

--- inc/woo/_hook_exts.php
+++ inc/woo/_hook_exts.php
@@ -20,7 +20,7 @@ if(class_exists('WPcleverWooscp')){
         function theclick_wooscp_icon(){
             global $product;
             $wooscp_text = apply_filters('wooscp_button_text', get_option( '_wooscp_button_text', esc_html__( 'Compare', 'theclick' ) ));
-            echo '<div class="woosmart-icon compare-icon hint--top-'.theclick_align2().'" data-selected="'.esc_html__('Open Compared List','theclick').'" data-hint="'.esc_html($wooscp_text).'">'.do_shortcode('[wooscp id="'.$product->get_id().'" type="link"]').'</div>';
+            echo '<div class="woosmart-icon compare-icon hint--top-'.theclick_align().'" data-selected="'.esc_html__('Open Compared List','theclick').'" data-hint="'.esc_html($wooscp_text).'">'.do_shortcode('[wooscp id="'.$product->get_id().'" type="link"]').'</div>';
         }
     }
 
@@ -46,7 +46,7 @@ if(class_exists('WPcleverWoosq') && $wc_quickview_to_attrs === '1'){
     add_action('theclick_woocommerce_loop_product_add_to_cart', function(){
         global $product;
         $woosq_text = apply_filters('woosq_button_text', get_option( 'woosq_button_text', esc_html__( 'Quick view', 'theclick' ) ));
-        echo '<div class="woosmart-icon quickview-icon hint--top-'.theclick_align2().'" data-hint="'.esc_html($woosq_text).'">'.do_shortcode('[woosq id="'.$product->get_id().'" type="link"]').'</div>';
+        echo '<div class="woosmart-icon quickview-icon hint--top-'.theclick_align().'" data-hint="'.esc_html($woosq_text).'">'.do_shortcode('[woosq id="'.$product->get_id().'" type="link"]').'</div>';
     },12);
 }
 /**
@@ -64,7 +64,7 @@ if(class_exists('WPcleverWoosw')){
 	        global $product;
             $woosw_text  = apply_filters( 'woosw_button_text', get_option( 'woosw_button_text', esc_html__( 'Add to Wishlist', 'theclick' ) ) );
 	        $woosw_text_added  = apply_filters( 'woosw_button_text_added', get_option( 'woosw_button_text_added', esc_html__( 'Browse Wishlist', 'theclick' ) ) );
-	        echo '<div class="woosmart-icon wishlist-icon hint--top-'.theclick_align2().'" data-selected="'.esc_html($woosw_text_added).'" data-hint="'.esc_html($woosw_text).'">'.do_shortcode('[woosw id="'.$product->get_id().'" type="link"]').'</div>';
+	        echo '<div class="woosmart-icon wishlist-icon hint--top-'.theclick_align().'" data-selected="'.esc_html($woosw_text_added).'" data-hint="'.esc_html($woosw_text).'">'.do_shortcode('[woosw id="'.$product->get_id().'" type="link"]').'</div>';
 	    },13);
 	}
     // Single
