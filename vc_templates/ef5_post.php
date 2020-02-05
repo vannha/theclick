7cf4af23ac70d6c9a14d2e63189625bd9c80f9ee
vannha <phongnhatk21@gmail.com>
2020-02-05 10:01:13 +0700

    commit -

--- vc_templates/ef5_post.php
+++ vc_templates/ef5_post.php
@@ -4,16 +4,21 @@
 	if(empty($post_id)) return;
  
 	$terms = get_the_term_list( $post_id , 'category', '', ' ', '' );
-	var_dump($terms);
+ 	
 	$post   = get_post( $post_id );
 ?>
 <div class="ef5-post <?php echo esc_attr($el_class);?>">
-    <?php theclick_post_media(['thumbnail_size' => 'medium']); ?>
+    <?php 
+    if(has_post_thumbnail() ){
+    	theclick_image_by_size(['id' => $post_id,'size' => $thumbnail_size, 'class' => $args['img_class']]);
+    }
+     
+    ?>
     <div class="ef5-post-info">
     	<div class="ef5-post-cat"><?php echo theclick_html($terms); ?></div>
     	<div class="ef5-post-title"><?php echo theclick_html($post->post_title); ?></div>
     	<?php
-        $content      = $post->post_excerpt;
+        $content      = $post->post_content;
         $excerpt_more = apply_filters('theclick_excerpt_more', '&hellip;');
         $excerpt      = wp_trim_words($content, '30', $excerpt_more);
         ?>
