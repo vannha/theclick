6e30e4d29d1be557b26214dc6fb67239ddc35b75
vannha <phongnhatk21@gmail.com>
2020-02-06 08:58:17 +0700

    commit -

--- template-parts/header/layout-2.php
+++ template-parts/header/layout-2.php
@@ -8,23 +8,27 @@
         <div class="<?php theclick_header_inner_class(); ?>">
             <div class="header-inner-wrap">
                 <div class="row justify-content-between align-items-center">
-                    <?php theclick_header_side_nav_icon(['class' => 'd-none d-sm-block']); ?>
-                    <div class="ef5-navigation-wrap col">
-                        <?php theclick_header_menu(['class' => 'ef5-navs']); ?>
+                    <div class="ef5-header-left col">
+                        <div class="d-flex align-items-center">
+                            <?php
+                            theclick_header_side_nav_icon(['class' => 'd-none d-xl-block']);
+                            theclick_header_menu(['class' => 'ef5-navs']); 
+                            ?>
+                        </div>
                     </div>
-                    <div class="ef5-logo col-auto">
-                        <?php get_template_part('template-parts/header/header-logo'); ?>
-                    </div> 
+                    <div class="ef5-navigation-wrap col-auto">
+                        <?php theclick_header_blog_menu(['class' => 'ef5-navs']); ?>
+                    </div>
                     <div class="header-attrs col">
                         <div class="header-attr-wrap nav-extra justify-content-end">
                             <?php
-                            theclick_header_signin_signup(['class' => 'd-none d-sm-block']);
-                            theclick_header_search(['class' => 'd-none d-sm-block']);
-                            theclick_header_wishlist(['class' => 'd-none d-sm-block']);
-                            theclick_header_compare(['class' => 'd-none d-sm-block']);
-                            theclick_header_cart(['class' => 'd-none d-sm-block']);
-                            theclick_header_social(['class' => 'd-none d-sm-block']);
-                            theclick_header_popup_nav_icon(['class' => 'd-none d-sm-block']);
+                            theclick_header_signin_signup(['class' => 'd-none d-md-block']);
+                            theclick_header_search(['class' => 'd-none d-md-block']);
+                            theclick_header_wishlist(['class' => 'd-none d-md-block']);
+                            theclick_header_compare(['class' => 'd-none d-md-block']);
+                            theclick_header_cart(['class' => 'd-none d-md-block']);
+                            theclick_header_social(['class' => 'd-none d-md-block']);
+                            theclick_header_popup_nav_icon(['class' => 'd-none d-md-block']);
                             theclick_header_mobile_menu_icon();
                             ?>
                         </div>
