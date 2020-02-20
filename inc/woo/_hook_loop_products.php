9d79c23b1e7b49f86ead1420a580c5de8874d385
vannha <phongnhatk21@gmail.com>
2020-02-20 14:45:07 +0700

    commit -

--- inc/woo/_hook_loop_products.php
+++ inc/woo/_hook_loop_products.php
@@ -197,5 +197,5 @@ function theclick_quantity_inputs_for_woocommerce_loop_add_to_cart_link( $html, 
 
 add_action('woocommerce_template_loop_rating1','theclick_woocommerce_template_loop_rating',0);
 function theclick_woocommerce_template_loop_rating(){
-	echo 'aaaaaaaa';
+	do_action('woocommerce_template_loop_rating');
 }
