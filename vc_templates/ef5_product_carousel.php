c0fe2580708cc57d500dbeaf005b5a475b4cb6d3
vannha <phongnhatk21@gmail.com>
2020-02-20 14:06:28 +0700

    commit -

--- vc_templates/ef5_product_carousel.php
+++ vc_templates/ef5_product_carousel.php
@@ -47,12 +47,12 @@
                 ?>
                 <div class="<?php echo trim(implode(' ', $item_css_class)); ?>">
                 <?php
-                    do_action( 'woocommerce_before_shop_loop_item' );
+                    //do_action( 'woocommerce_before_shop_loop_item' );
                     //do_action( 'woocommerce_before_shop_loop_item_title' );
                     echo woocommerce_get_product_thumbnail();
                     do_action( 'woocommerce_shop_loop_item_title' );
-                    do_action( 'woocommerce_after_shop_loop_item_title' );
-                    do_action( 'woocommerce_after_shop_loop_item' );
+                    //do_action( 'woocommerce_after_shop_loop_item_title' );
+                    //do_action( 'woocommerce_after_shop_loop_item' );
                 ?>
                 </div>
             <?php
