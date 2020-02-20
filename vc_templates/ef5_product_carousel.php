2aca2ac91f792b6de5aad4fd550c4d8542812b71
vannha <phongnhatk21@gmail.com>
2020-02-20 11:25:28 +0700

    commit -

--- vc_templates/ef5_product_carousel.php
+++ vc_templates/ef5_product_carousel.php
@@ -18,13 +18,13 @@
 
     $posts = theclick_woocommerce_query($type,$number,$product_ids,$taxonomies, $taxonomies_exclude,$category_slug); 
     $count = $posts->post_count;
-    var_dump($count);
+    var_dump($posts);
     $grid_item_css_class = ['ef5-post-item', 'ef5-post-item-layout-'.$layout_template, 'ef5-carousel-item'];
     
     $item_css_class = ['ef5-post-item-inner','transition'];
 
     
-    while($posts->have_posts()){
+    /*while($posts->have_posts()){
       
 	    switch ($layout_template) {
 	        case '1':
@@ -35,7 +35,7 @@
 	        break;
 	    }
     
-    } // end while
-    wp_reset_postdata();
+    }  
+    wp_reset_postdata();*/
 ?>  
 
