f18a6eea98f291c36b24a3a30252e78821fc81bf
vannha <phongnhatk21@gmail.com>
2020-02-20 13:42:53 +0700

    commit -

--- vc_templates/ef5_product_carousel.php
+++ vc_templates/ef5_product_carousel.php
@@ -44,9 +44,15 @@
             <?php
             switch ($layout_template) {
                 case '1':
-                ?>	
-            	<div class="<?php echo trim(implode(' ', $item_css_class)); ?>">
-                     bbbbbbbbbbbbbbbbbb
+                ?>
+                <div class="<?php echo trim(implode(' ', $item_css_class)); ?>">
+                <?php
+                    do_action( 'woocommerce_before_shop_loop_item' );
+                    do_action( 'woocommerce_before_shop_loop_item_title' );
+                    do_action( 'woocommerce_shop_loop_item_title' );
+                    do_action( 'woocommerce_after_shop_loop_item_title' );
+                    do_action( 'woocommerce_after_shop_loop_item' );
+                ?>
                 </div>
             <?php
                 break;
