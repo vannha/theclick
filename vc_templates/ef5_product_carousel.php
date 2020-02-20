948ab760d65b62ea5c557f1601dbb24963588d2d
vannha <phongnhatk21@gmail.com>
2020-02-20 11:33:03 +0700

    commit -

--- vc_templates/ef5_product_carousel.php
+++ vc_templates/ef5_product_carousel.php
@@ -23,14 +23,15 @@
         'orderby' => 'date',
         'order' => 'DESC'
     );
-    $wp_query = new WP_Query($args);
+    //$wp_query = new WP_Query($args);
     //$wp_query = theclick_woocommerce_query($type,$number,$product_ids,$taxonomies, $taxonomies_exclude,$category_slug); 
-    $count = $wp_query->post_count;
-    var_dump($count);
+    //$count = $wp_query->post_count;
+    //var_dump($count);
     $grid_item_css_class = ['ef5-post-item', 'ef5-post-item-layout-'.$layout_template, 'ef5-carousel-item'];
     
     $item_css_class = ['ef5-post-item-inner','transition'];
 
+    echo 'aaaaaaaaaaa';
     
     /*while($posts->have_posts()){
       
