03c4aefa47bd4d0e136c54338fa1ab4d272238f1
vannha <phongnhatk21@gmail.com>
2020-02-20 08:35:29 +0700

    commit -

--- inc/functions/product.php
+++ inc/functions/product.php
@@ -1,7 +1,7 @@
 <?php
 function theclick_woocommerce_query($type,$post_per_page=-1,$product_ids='',$taxonomies='', $taxonomies_exclude='', $product_cat=''){
     global $wp_query;
-	$args = theclick_woocommerce_query_args($type,$post_per_page,$product_ids,$taxonomies, $taxonomies_exclude, $product_cat);
+	$args = []; //theclick_woocommerce_query_args($type,$post_per_page,$product_ids,$taxonomies, $taxonomies_exclude, $product_cat);
 
     if (get_query_var('paged')){ 
     	$paged = get_query_var('paged'); 
@@ -16,7 +16,7 @@ function theclick_woocommerce_query($type,$post_per_page=-1,$product_ids='',$tax
     	$args['paged'] = $paged;
     }
 
-    var_dump($args); die;
+
     $wp_query = new WP_Query($args);
 	return $wp_query;
 }
