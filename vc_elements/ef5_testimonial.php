a3bd73315a6696bba09de4150aadc78548065d61
vannha <phongnhatk21@gmail.com>
2020-02-24 14:42:14 +0700

    commit -

---
+++ vc_elements/ef5_testimonial.php
@@ -0,0 +1,286 @@
+<?php
+vc_map(array(
+    'name'        => 'Theclick Testimonial',
+    'base'        => 'ef5_testimonial',
+    'icon'        => 'ef5-icon-quote',
+    'category'    => esc_html__('TheClick', 'theclick'),
+    'description' => esc_html__('Add clients testimonial', 'theclick'),
+    'params'      => array_merge(
+        array(
+            array(
+                'type'        => 'textfield',
+                'heading'     => esc_html__( 'Title', 'theclick' ),
+                'param_name'  => 'title',
+                'value'       => '',
+            ),
+            array(
+                'type'          => 'dropdown',
+                'heading'       => esc_html__('Content Align','theclick'),
+                'param_name'    => 'content_align',
+                'value'         => array(
+                    'Default'       => '',
+                    'Text Left'     => 'text-start',
+                    'Text Right'    => 'text-end',
+                    'Text Center'   => 'text-center',
+                ),
+                'std'           => '',
+            ),
+            array(
+                'type'       => 'img',
+                'heading'    => esc_html__('Layout Template','theclick'),
+                'param_name' => 'layout_template',
+                'value'      =>  array(
+                    '1' => get_template_directory_uri().'/vc_elements/layouts/testimonial-1.jpg',
+                ),
+                'std'         => '1',
+                'admin_label' => true
+            ),
+            // Text Color
+            array(
+                'type'         => 'dropdown',
+                'heading'      => esc_html__( 'Text Color', 'theclick' ),
+                'param_name'   => 'text_color_opts',
+                'value'        => ef5systems_colors_option_for_vc([
+                    esc_html__('Custom','theclick') => 'custom'
+                ]),
+                'std'          => '',
+                'description'  => esc_html__( 'Choose color for this row', 'theclick' ),
+                'edit_field_class' => 'vc_col-sm-6',
+            ),
+            array(
+                'type'         => 'colorpicker',
+                'heading'      => esc_html__( 'Custom Text Color', 'theclick' ),
+                'param_name'   => 'text_color',
+                'description'  => esc_html__( 'Choose your color for this row', 'theclick' ),
+                'dependency' => array(
+                    'element'   => 'text_color_opts',
+                    'value'     => 'custom'
+                ),
+                'edit_field_class' => 'vc_col-sm-6',
+            ),
+            array(
+                'type'        => 'el_id',
+                'settings' => array(
+                    'auto_generate' => true,
+                ),
+                'heading'     => esc_html__( 'Element ID', 'theclick' ),
+                'param_name'  => 'el_id',
+                'description' => sprintf( __( 'Enter element ID (Note: make sure it is unique and valid according to <a href="%s" target="_blank">w3c specification</a>).', 'theclick' ), '//w3schools.com/tags/att_global_id.asp' ),
+            ),
+            array(
+                'type'       => 'textfield',
+                'heading'    => esc_html__('Element Class','theclick'),
+                'param_name' => 'el_class',
+                'value'      => '',
+                'description' => esc_html__('Style particular content element differently - add a class name and refer to it in custom CSS.', 'theclick'),
+            ),
+            /* Testimonial Settings */
+            array(
+                'type'          => 'param_group',
+                'heading'       => esc_html__( 'Add your testimonial', 'theclick' ),
+                'param_name'    => 'testimonials',
+                'value'         => urlencode( json_encode( array(
+                    array(
+                        'author_name'          => 'Andrew Bright',
+                        'author_position'      => 'CEO & Founder',
+                        'author_url'           => '#',
+                        'author_avatar'        => '',
+                        'author_signature_img' => '',
+                        'text'                 => ''
+                    ),
+                    array(
+                        'author_name'          => 'Conrad Smith',
+                        'author_position'      => 'Volunteer',
+                        'author_url'           => '#',
+                        'author_avatar'        => '',
+                        'author_signature_img' => '',
+                        'text'                 => ''
+                    ),
+                    array(
+                        'author_name'          => 'Belle Hill',
+                        'author_position'      => 'Volunteer',
+                        'author_url'           => '#',
+                        'author_avatar'        => '',
+                        'author_signature_img' => '',
+                        'text'                 => ''
+                    ),
+                    array(
+                        'author_name'          => 'Alison Becker',
+                        'author_position'      => 'Manager',
+                        'author_url'           => '#',
+                        'author_avatar'        => '',
+                        'author_signature_img' => '',
+                        'text'                 => ''
+                    )
+                ) ) ),
+                'params' => array(
+                    array(
+                        'type'          => 'textfield',
+                        'heading'       => esc_html__( 'Author name', 'theclick' ),
+                        'param_name'    => 'author_name',
+                        'std'			=> 'John Smith',
+                        'admin_label'   => true,
+                    ),
+                    array(
+                        'type'          => 'textfield',
+                        'heading'       => esc_html__( 'Author Position', 'theclick' ),
+                        'param_name'    => 'author_position',
+                        'std'			=> 'Project Manager',
+                    ),
+                    array(
+                        'type'          => 'textfield',
+                        'heading'       => esc_html__( 'Author URL', 'theclick' ),
+                        'param_name'    => 'author_url',
+                        'std'			=> '#',
+                    ),
+                    array(
+                        'type'          => 'attach_image',
+                        'heading'       => esc_html__( 'Author Image', 'theclick' ),
+                        'param_name'    => 'author_avatar',
+                        'value'         => '',
+                        'edit_field_class' => 'vc_col-sm-6'
+                    ),
+                    array(
+                        'type'          => 'attach_image',
+                        'heading'       => esc_html__( 'Author signature image', 'theclick' ),
+                        'param_name'    => 'author_signature_img',
+                        'value'         => '',
+                        'edit_field_class' => 'vc_col-sm-6'
+                    ),
+                    array(
+		                'type'          => 'dropdown',
+		                'heading'       => esc_html__( 'Star Rate', 'theclick' ),
+		                'param_name'    => 'author_rate',
+		                'value'         => array('','1','2','3','4','5'),
+		                'std'           => ''
+		            ),
+                    array(
+                        'type'          => 'textarea',
+                        'heading'       => esc_html__( 'Testimonial text', 'theclick' ),
+                        'description'   => esc_html__('Press double ENTER to get line-break','theclick'),
+                        'param_name'    => 'text',
+                        'std'           => 'Donec euismod sem ac urna finibus, sit amet efficitur erat tem pus. Ut dapibus dictum turpis, vel faucibus erat posuere vitae icitur erat tem puna'
+                    ),
+                ),
+                'group' => esc_html__('Testimonial Item','theclick')
+            ),
+            array(
+                'type'       => 'dropdown',
+                'heading'    => esc_html__('Layout Style','theclick'),
+                'param_name' => 'layout_style',
+                'value'      =>  array(
+                    esc_html__('Grid','theclick')     => 'grid',
+                    esc_html__('Carousel','theclick') => 'carousel'
+                ),
+                'std'        => 'grid',
+                'group'      => esc_html__('Layout Settings','theclick'),
+                'admin_label'=> true
+            )
+        ),
+        /* Grid settings */
+        ef5systems_grid_settings(array(
+            'group'                  => esc_html__('Layout Settings','theclick'), 
+            'dependency_element'     => 'layout_style', 
+            'dependency_value_value' => 'grid'
+            )
+        ),
+        /* Carousel Settings */
+        ef5systems_owl_settings(array(
+            'group'      => esc_html__('Layout Settings','theclick'), 
+            'param_name' => 'layout_style', 
+            'value'      => 'carousel'
+            )
+        )
+    )
+));
+
+class WPBakeryShortCode_ef5_testimonial extends WPBakeryShortCode
+{
+    protected function content($atts, $content = null){
+        $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+        ef5systems_owl_call_settings($atts);
+        return parent::content($atts, $content);
+    }
+    protected function overcome_tm_text_color($atts, $args = []){
+        extract($atts);
+        $args = wp_parse_args($args, [
+            'echo' => true
+        ]);
+        $color = (!empty($text_color_opts) && !empty($text_color)) ? 'style="color:'.$text_color.'"' : '';
+        if($args['echo'])
+            echo overcome_html($color);
+        else 
+            return $color;
+    }
+    protected function overcome_tm_text($testimonial, $atts, $args=[]){
+        if(empty($testimonial['text'])) return;
+        $args = wp_parse_args($args,[
+            'class' => ''
+        ]);
+        $classes = ['ttmn-text', 'text-'.$atts['text_color_opts'], $args['class']];
+        ?>
+            <div class="<?php echo trim(implode(' ', $classes));?>" <?php $this->overcome_tm_text_color($atts);?>>
+                <?php echo overcome_html($testimonial['text']);?>
+            </div>
+        <?php
+    }
+    protected function overcome_tm_name($testimonial, $atts, $args=[]){
+        if(empty($testimonial['author_name'])) return;
+        $args = wp_parse_args($args,[
+            'class' => ''
+        ]);
+        $classes = ['ttmn-name', 'text-'.$atts['text_color_opts'], $args['class']];
+        $author_link_open = $author_link_close = '';
+        if(!empty($testimonial['author_url'])){
+            $author_link_open = '<a class="ef5-text-inherit" href="'.esc_url($testimonial['author_url']).'" target="_blank">';
+            $author_link_close = '</a>';
+        }
+        ?>
+            <span class="<?php echo trim(implode(' ', $classes));?>" <?php $this->overcome_tm_text_color($atts);?>>
+                <?php echo overcome_html($author_link_open.$testimonial['author_name'].$author_link_close);?>
+            </span>
+        <?php
+    }
+    protected function overcome_tm_position($testimonial, $args=[]){
+        if(empty($testimonial['author_position'])) return;
+        $args = wp_parse_args($args,[
+            'class' => 'ef5-text-accent'
+        ]);
+        $classes = ['ttmn-position', $args['class']];
+        ?>
+            <span class="<?php echo trim(implode(' ', $classes));?>">
+                <?php echo overcome_html($testimonial['author_position']);?>
+            </span>
+        <?php
+    }
+    protected function overcome_tm_rate($testimonial,$atts, $args=[]){
+        if(empty($testimonial['author_rate'])) return;
+        $args = wp_parse_args($args,[
+            'class' => ''
+        ]);
+        $classes = ['ttmn-rate', $args['class']];
+        $author_rate = ($testimonial['author_rate']/5)*100;
+        ?>
+            <div class="<?php echo trim(implode('', $classes));?>"><span class="ttmn-rated" style="width:<?php echo esc_attr($author_rate);?>%"></span></div>
+        <?php
+    }
+    protected function overcome_tm_avatar($testimonial,$atts, $args=[]){
+        $testimonial['author_avatar'] = isset($testimonial['author_avatar']) ? $testimonial['author_avatar'] : null;
+        $args = wp_parse_args($args,[
+            'img_class' => '',
+            'size'      => '90',
+            'before'    => '<div class="tm-avatar">',
+            'after'     => '</div>'
+        ]);
+        $classes = ['avatar w-auto', $args['img_class']];
+
+        overcome_image_by_size([
+            'id'      => $testimonial['author_avatar'],
+            'size'    => $args['size'],
+            'class'   => trim(implode(' ', $classes)),
+            'default' => true,
+            'before'  => $args['before'],
+            'after'   => $args['after']  
+        ]);
+    }
+}
---
+++ vc_templates/ef5_testimonial.php
@@ -0,0 +1,187 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    die( '-1' );
+}
+/**
+ * Shortcode attributes
+ * @var $atts
+ * Shortcode class
+ * @var $this WPBakeryShortCode_ef5_testimonial
+ */
+$atts = vc_map_get_attributes( $this->getShortcode(), $atts );
+extract( $atts );
+$el_id = !empty($el_id) ? 'ef5-'.$el_id : uniqid('ef5-');
+$wrap_css_class = ['ef5-testimonial-wrap','ef5-owl-wrap-inner relative'];
+$css_class_attr = $item_class = array();
+$css_class_attr[] = 'ef5-testimonial ef5-testimonial-layout-'.$layout_template;
+$item_class[] = 'testimonial-item';
+
+if($layout_style === 'carousel'){
+    $wrap_css_class[] = ef5systems_owl_css_class($atts);
+    $css_class_attr[] = 'ef5-owl testimonial-carousel owl-carousel';
+    $item_class[] = 'ef5-carousel-item';
+} else {
+    $css_class_attr[] = 'ef5-grid row justify-content-center';
+    $item_class[] = 'ef5-grid-item col-'.$col_sm.' col-md-'.$col_md.' col-lg-'.$col_lg.' col-xl-'.$col_xl;
+}
+
+$css_class_attr[] = $content_align;
+$css_class_attr[] = $el_class;
+
+/* get space for owl item */
+$owl_item_space = '';
+if(isset($margin) && (isset($number_row) && $number_row > 1 )){
+    $owl_item_space = 'style="margin-bottom:'.$margin.'px"';
+}
+// get testinomial data
+$testimonials = (array) vc_param_group_parse_atts( $atts['testimonials'] );
+// avatar size
+switch ($layout_template) {
+	default:
+		$dot_thumbnail_size = '70';
+		break;
+}
+$count = count($testimonials);
+$i=1;
+$j=0;
+
+$inner_css_classes = ['ttmn-inner','transition'];
+
+$ttmn_wrap_classes = ['ef5-testimonials', ef5systems_owl_css_class($atts)];
+
+if(empty($atts['content_align']) && !in_array($atts['layout_template'],['2','5','6'])) $ttmn_wrap_classes[] = 'text-center';
+?>
+<div class="<?php echo overcome_optimize_css_class(implode(' ', $ttmn_wrap_classes));?>">
+    <?php 
+        ef5systems_owl_nav_top($atts);
+        ef5systems_owl_dots_top($atts); 
+    ?>
+    <div class="<?php echo trim(implode(' ', $wrap_css_class));?>">
+        <div id="<?php echo esc_attr($el_id);?>" class="<?php echo trim(implode(' ',$css_class_attr));?>"> 
+            <?php
+                foreach($testimonials as $testimonial){
+                    $j++;
+                    if($i > $number_row) $i=1;
+
+                    if(isset($testimonial['author_name'])) {
+                    	// dot image
+                    	$dot_image = overcome_image_by_size([
+    						'id'    => isset($testimonial['author_avatar']) ? $testimonial['author_avatar'] : null,
+    						'size'  => $dot_thumbnail_size, 
+    						'class' => 'dot-thumb circle', 
+    						'echo'  => false
+                    	]);
+                        // star rating
+                        $testimonial['author_rate'] = isset($testimonial['author_rate']) ? $testimonial['author_rate'] : '';
+                        if($i==1) : ?>
+                            <div class="<?php echo join(' ',$item_class);?>" data-dot='<?php echo overcome_html($dot_image); ?>'>
+                        <?php  
+                            endif;
+                            echo '<div class="'.trim(implode(' ', $inner_css_classes)).'" '.$owl_item_space.'>';
+                            	switch ($layout_template) {
+                                    case '6':
+                                        // text 
+                                        $this->overcome_tm_text($testimonial, $atts,['class' => 'text-13 ef5-text-777777 font-style-400 pb-18']);
+                                        // name
+                                        $this->overcome_tm_name($testimonial, $atts,['class' => 'font-style-500 ef5-text-accent d-block']);
+                                        // position
+                                        $this->overcome_tm_position($testimonial,['class' => 'text-13 ef5-text-777777 d-block']);
+                                        // star rating
+                                        $this->overcome_tm_rate($testimonial, $atts);
+                                        //avatar
+                                        $this->overcome_tm_avatar($testimonial,$atts,['size' => '65', 'img_class' => 'circle']);
+                                    break;
+                                    case '5':
+                                    ?>
+                                        <div class="row getters-20 align-items-center">
+                                            <div class="col-auto">
+                                            <?php //avatar
+                                                $this->overcome_tm_avatar($testimonial,$atts,['size' => '65', 'img_class' => 'circle']); ?>
+                                            </div>
+                                            <div class="col">
+                                                <?php // name
+                                                $this->overcome_tm_name($testimonial, $atts,['class' => 'font-style-500 d-block']);
+                                                // position
+                                                $this->overcome_tm_position($testimonial,['class' => 'text-13 d-block ef5-text-777777']);
+                                                // star rating
+                                                $this->overcome_tm_rate($testimonial, $atts); ?>
+                                            </div>
+                                        </div>
+                                    <?php
+                                        // text 
+                                        $this->overcome_tm_text($testimonial, $atts,['class' => 'ef5-text-777777 font-style-300 pt-25 pl-30 pb-18']);
+                                    break;
+                                    case '4':
+                                        //avatar
+                                        $this->overcome_tm_avatar($testimonial,$atts,['size' => '73', 'img_class' => 'mb-20 circle ml-auto mr-auto']);
+                                        // name
+                                        $this->overcome_tm_name($testimonial, $atts,['class' => 'font-style-500 d-block']);
+                                        // position
+                                        $this->overcome_tm_position($testimonial,['class' => 'text-13 ef5-text-accent d-block']);
+                                        // star rating
+                                        $this->overcome_tm_rate($testimonial, $atts);
+                                        // text 
+                                        $this->overcome_tm_text($testimonial, $atts,['class' => 'text-22 font-style-300 pt-35']);
+                                    break;
+                                    case '3':
+                                        // text 
+                                        $this->overcome_tm_text($testimonial, $atts,['class' => 'text-20 font-style-300 pb-40']);
+                                        //avatar
+                                        $this->overcome_tm_avatar($testimonial,$atts,['size' => '80', 'img_class' => 'mb-20 circle ml-auto mr-auto']);
+                                        // name
+                                        $this->overcome_tm_name($testimonial, $atts,['class' => 'text-22 font-style-400 d-block']);
+                                        // position
+                                        $this->overcome_tm_position($testimonial,['class' => 'text-12 text-uppercase d-block text-'. $atts['text_color_opts']]);
+                                        // star rating
+                                        $this->overcome_tm_rate($testimonial, $atts);
+                                    break;
+                                    case '2' :
+                                        echo '<div class="ef5-bg-white ef5-rounded-10 p-25">';
+                                            echo '<div class="ttmn-header row gutters-20 align-items-center pb-25">';
+                                                echo '<div class="col-auto">';
+                                                    $this->overcome_tm_avatar($testimonial,$atts,[
+                                                        'size'      => '65', 
+                                                        'img_class' => 'circle'
+                                                    ]);
+                                                echo '</div>';
+                                                echo '<div class="col">';
+                                                    // name
+                                                    $this->overcome_tm_name($testimonial, $atts,['class' => 'font-style-500 d-block']);
+                                                    // position
+                                                    $this->overcome_tm_position($testimonial,['class' => 'text-13 ef5-text-787878 d-block']);
+                                                    // star rating
+                                                    $this->overcome_tm_rate($testimonial, $atts);
+                                                echo '</div>';
+                                            echo '</div>';
+                                            // text 
+                                            $this->overcome_tm_text($testimonial, $atts,['class' => 'pb-17 pl-lg-25 pr-lg-25 ef5-text-787878']);
+                                        echo '</div>';
+                                    break;
+                            		default:
+                            			// text 
+                                        $this->overcome_tm_text($testimonial, $atts,['class' => 'text-22 font-style-300 pb-40']);
+                            			//avatar
+                            			$this->overcome_tm_avatar($testimonial,$atts,['size' => '73', 'img_class' => 'mb-20 circle ml-auto mr-auto']);
+                                        // name
+                                        $this->overcome_tm_name($testimonial, $atts,['class' => 'font-style-500 d-block']);
+                                        // position
+                                        $this->overcome_tm_position($testimonial,['class' => 'text-13 ef5-text-accent d-block']);
+                                        // star rating
+                                        $this->overcome_tm_rate($testimonial, $atts);
+                            		break;
+                            	}
+                            echo '</div>';
+                        if($i == $number_row || $j==$count) echo '</div>';
+                        $i ++;
+                    }
+                }
+            ?>
+        </div>
+        <?php if($layout_style === 'carousel'):
+            overcome_loading_animation(); 
+            ef5systems_owl_dots_container($atts);
+            ef5systems_owl_nav_container($atts);
+            ef5systems_owl_dots_in_nav_container($atts);
+        endif; ?>
+    </div>
+</div>
