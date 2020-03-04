ddc4fa0ad67ade4aa46c7c301b4fd34f12b3bef6
vannha <phongnhatk21@gmail.com>
2020-03-04 14:39:25 +0700

    commit -

--- vc_templates/ef5_product_filter.php
+++ vc_templates/ef5_product_filter.php
@@ -31,7 +31,7 @@
             foreach($filter_type as $ft): 
                 if( !empty($ft['filter_type_item']) ){
                 	$title = !empty($ft['filter_title_item']) ? $ft['filter_title_item'] : $default_title[$ft['filter_type_item']];
-                    echo '<a href="'.esc_url(get_page_link(false).'filter_type='.$ft['filter_title_item']).'" class="aaaa">'.$title.'</a>';
+                    echo '<a href="'.esc_url(get_page_link(false).'filter_type='.$ft['filter_type_item']).'" class="aaaa">'.$title.'</a>';
                 }
             endforeach; 
             ?>
