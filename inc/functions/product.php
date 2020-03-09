64a9a0dfe409cce228f063cdb6bb51eaefe4f5a9
vannha <phongnhatk21@gmail.com>
2020-03-09 16:14:59 +0700

    commit -

--- inc/functions/product.php
+++ inc/functions/product.php
@@ -228,12 +228,9 @@ function theclick_product_filter_sidebar(){
                 
             $current_min_price = isset( $_GET['min_price'] ) ? floor( floatval( wp_unslash( $_GET['min_price'] ) ) / $step ) * $step : $min_price; // WPCS: input var ok, CSRF ok.
             $current_max_price = isset( $_GET['max_price'] ) ? ceil( floatval( wp_unslash( $_GET['max_price'] ) ) / $step ) * $step : $max_price; 
-          var_dump([$min_price,$max_price,$current_min_price,$current_max_price]);
-            /*$step = '';
-            $min_price = '';
-            $max_price = '';
-            $current_min_price = '';
-            $current_max_price = '';*/
+    
+            $current_min_price = 10;
+            $current_max_price = 50;
             ?>
             <div class="filter price_slider_wrapper">
                 <div class="price_slider" style="display:none;"></div>
