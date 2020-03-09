3d00ce48f2fbad6f648ae124edafedd13e16dbe1
vannha <phongnhatk21@gmail.com>
2020-03-09 15:06:36 +0700

    commit -

--- inc/functions/product.php
+++ inc/functions/product.php
@@ -153,6 +153,14 @@ function theclick_product_filter_sidebar(){
             wp_register_script( 'accounting', WC()->plugin_url() . '/assets/js/accounting/accounting.min.js', array( 'jquery' ), '0.4.2', true );
             wp_register_script( 'wc-jquery-ui-touchpunch', WC()->plugin_url() . '/assets/js/jquery-ui-touch-punch/jquery-ui-touch-punch.min.js', array( 'jquery-ui-slider' ), WC_VERSION, true );
             wp_register_script( 'wc-price-slider', WC()->plugin_url() . '/assets/js/frontend/price-slider.min.js', array( 'jquery-ui-slider', 'wc-jquery-ui-touchpunch', 'accounting' ), WC_VERSION, true );
+            $abc = array(
+                    'currency_format_num_decimals' => 0,
+                    'currency_format_symbol'       => get_woocommerce_currency_symbol(),
+                    'currency_format_decimal_sep'  => esc_attr( wc_get_price_decimal_separator() ),
+                    'currency_format_thousand_sep' => esc_attr( wc_get_price_thousand_separator() ),
+                    'currency_format'              => esc_attr( str_replace( array( '%1$s', '%2$s' ), array( '%s', '%v' ), get_woocommerce_price_format() ) ),
+                );
+            var_dump($abc);            
             wp_localize_script(
                 'wc-price-slider',
                 'woocommerce_price_slider_params',
