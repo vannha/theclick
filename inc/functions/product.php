49a80f3326ffc877efe68fbc110a555365a881de
vannha <phongnhatk21@gmail.com>
2020-03-10 08:21:53 +0700

    commit -

--- inc/functions/product.php
+++ inc/functions/product.php
@@ -213,12 +213,10 @@ function theclick_product_filter_sidebar(){
                     <div class="price_slider_amount" data-step="<?php echo esc_attr( $step ); ?>">
                         <input type="text" id="min_price" name="min_price" value="<?php echo esc_attr( $current_min_price ); ?>" data-min="<?php echo esc_attr( $min_price ); ?>" placeholder="<?php echo esc_attr__( 'Min price', 'theclick' ); ?>" />
                         <input type="text" id="max_price" name="max_price" value="<?php echo esc_attr( $current_max_price ); ?>" data-max="<?php echo esc_attr( $max_price ); ?>" placeholder="<?php echo esc_attr__( 'Max price', 'theclick' ); ?>" />
-                        <?php /* translators: Filter: verb "to filter" */ ?>
                         <button type="submit" class="button"><?php echo esc_html__( 'Filter', 'theclick' ); ?></button>
                         <div class="price_label" style="display:none;">
                             <?php echo esc_html__( 'Price:', 'theclick' ); ?> <span class="from"></span> &mdash; <span class="to"></span>
                         </div>
-                        <?php //echo wc_query_string_form_fields( null, array( 'min_price', 'max_price', 'paged' ), '', true ); ?>
                         <div class="clear"></div>
                     </div>
                 </div>
