601ee3846cadcf2b42ae9933e7c27f3b26accfb9
vannha <phongnhatk21@gmail.com>
2020-03-10 16:07:34 +0700

    commit -

--- inc/functions/product.php
+++ inc/functions/product.php
@@ -273,7 +273,7 @@ function theclick_ef5_product_filter_action_callback(){
             'post_status'    => 'publish',
             'post_parent'    => 0
         ); 
-        if (get_query_var('paged')){ 
+        /*if (get_query_var('paged')){ 
             $paged = get_query_var('paged'); 
         }elseif(get_query_var('page')){ 
             $paged = get_query_var('page'); 
@@ -282,7 +282,7 @@ function theclick_ef5_product_filter_action_callback(){
         }
         if($paged > 1){
             $args['paged'] = $paged;
-        }
+        }*/
 
         $grid_item_css_class = ['ef5-grid-item-wrap', 'col-' . $col_sm, 'col-md-' . $col_md, 'col-lg-' . $col_lg, 'col-xl-' . $col_xl];
 
