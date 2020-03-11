5f9608e74363fac802305a405f690d041c72d7eb
vannha <phongnhatk21@gmail.com>
2020-03-11 08:51:32 +0700

    commit -

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -829,7 +829,7 @@
             var $grid_inner = $(document).find('.ef5-product-grid.grid-filter .ef5-product-grid-content .ef5-product-grid-wrap');
             $.get(url, function (data) {
                 $('.ef5-wc-loop-img').slick('unslick');
-                $grid_inner.append($(data).find('.ef5-product-grid.grid-filter .ef5-product-grid-wrap .ef5-grid-item-wrap');
+                $grid_inner.append($(data).find('.ef5-product-grid.grid-filter .ef5-product-grid-wrap .ef5-grid-item-wrap'));
                 $grid_inner.imagesLoaded( function() {
                     $('.ef5-wc-loop-img').not('.slick-initialized').slick({
                         vertical: false,
