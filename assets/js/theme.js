efb716808f26602e9e30e41bbe5fe22560578fa2
vannha <phongnhatk21@gmail.com>
2020-03-16 08:37:26 +0700

    commit -

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -827,7 +827,7 @@
             $('.filter-by-sidebar .overlay-wrap').hide('slow');
         });
         
-        $('.filter-by-sidebar .filter-control .wc-enhanced-select').select2();
+        $('.wc-enhanced-select').select2();
  
         $(document).on('click','.ef5-ajax-filter', function() {
             var form1 = $(this).closest('.ajax-filter');
