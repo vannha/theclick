ac35f34781e75570a4805c8ff6b0737197c89c39
vannha <phongnhatk21@gmail.com>
2020-03-21 08:43:29 +0700

    commit -

--- inc/woo/_hook_price_html.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!function_exists('theclick_woocommerce_get_price_html')){
-	add_filter('woocommerce_get_price_html', 'theclick_woocommerce_get_price_html', 10, 1);
-	function theclick_woocommerce_get_price_html($price){
-		$class = is_singular('product') ? 'single' : 'loop';
-		return '<div class="ef5-products-price '.esc_attr($class).'">'.$price.'</div>';
-	}
-}
