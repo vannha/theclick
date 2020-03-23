ce9e87bbf51d825000dddc45613b64ed91ca0b37
vannha <phongnhatk21@gmail.com>
2020-03-23 16:21:46 +0700

    commit -

--- inc/functions/_instagram.php
+++ inc/functions/_instagram.php
@@ -207,6 +207,36 @@ if (!function_exists('theclick_instagram_html_output')) {
                 <?php 
                 echo '</div>';
             break;
+            case 'carousel':
+
+                echo '<div class="ef5-instagram layout-' . $layout_mode . ' ' . $less_more . '">'; echo 'aaaaaaa';
+                ?>
+                <div class="ef5-instagram-wrap row grid-gutters-<?php echo esc_attr($columns_space); ?> clearfix">
+                    <?php
+                    foreach ($media_array['images'] as $item) {
+                        ?>
+                        <div class="<?php echo trim(implode(' ', array('instagram-item', $span, 'overlay-wrap'))); ?>">
+                            <a class="ins-img" href="<?php echo esc_url($item['link']); ?>" target="<?php echo esc_attr($target); ?>">
+                                <img src="<?php echo esc_url($item[$size]); ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" />
+                            </a>
+                            <div class="overlay d-flex align-items-center animated" data-animation-in="zoomIn" data-animation-out="zoomOut">
+                                <div class="overlay-inner col-12 text-center">
+                                    <a class="ins-icon" href="<?php echo esc_url($item['link']); ?>" target="<?php echo esc_attr($target); ?>"><span class="fa fa-instagram"></span></a>
+                                    <?php if ($show_like) : ?><a class="like" href="<?php echo esc_url($item['link']); ?>" target="<?php echo esc_attr($target); ?>"><span class="fa fa-heart-o"></span><span><?php echo esc_html($item['likes']); ?></span></a><?php endif; ?>
+                                    <?php if ($show_cmt) : ?><a class="comments" href="<?php echo esc_url($item['link']); ?>" target="<?php echo esc_attr($target); ?>"><span class="fa fa-comments-o"></span><span><?php echo esc_html($item['comments']); ?></span></a><?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+                <?php if ($show_author) { ?>
+                    <div class="user"><a href="//instagram.com/<?php echo trim($username); ?>" target="<?php echo esc_attr($target); ?>"><?php if (!empty($author_text)) echo '<span class="author-text">' . esc_html($author_text) . '</span>';
+                        else echo '<span class="author-text">' . $display_name . '</span>';
+                    echo '<span class="author-name">@' . trim($username) . '</span>'; ?></a></div>
+                <?php
+                }
+                echo '</div>';
+            break;
             default:
                 echo '<div class="ef5-instagram layout-' . $layout_mode . ' ' . $less_more . '">';
                 ?>
