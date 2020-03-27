1ea57cc10b5ad380b0278e4b3a60655ddc78bd81
vannha <phongnhatk21@gmail.com>
2020-03-27 15:23:13 +0700

    commit -

--- vc_templates/ef5_banner.php
+++ vc_templates/ef5_banner.php
@@ -36,9 +36,9 @@ $el_id = !empty($el_id) ? 'ef5-banner-' . $el_id : uniqid('ef5-banner-');
             echo '</div>';
             break;
         case '5': 
-            echo '<div class="row align-items-center gutter-0">'; 
-            $this->theclick_banner_main_media($atts,['class' => 'col-12 col-md-7 col-lg-6 col-xl-6 col-xxl-auto']);
-            echo '<div class="content-wrap col-12 col-md-5 col-lg-6 col-xl-6 col-xxl-auto">'; 
+            echo '<div class="row align-items-centers gutter-0">'; 
+            $this->theclick_banner_main_media($atts,['class' => '']);
+            echo '<div class="content-wrap">'; 
             $this->theclick_banner_sub_title($atts,['class' => '']);
             $this->theclick_banner_main_title($atts,['class' => '']);
             $this->theclick_banner_button($atts,['class' => 'btn-link']);
