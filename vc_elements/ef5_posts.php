c8a1c1c56e55b84fe46ee37aa31f83577ab0c102
vannha <phongnhatk21@gmail.com>
2020-03-31 11:25:38 +0700

    commit -

--- vc_elements/ef5_posts.php
+++ vc_elements/ef5_posts.php
@@ -82,6 +82,7 @@ vc_map(array(
                     '1'  => get_template_directory_uri().'/vc_elements/layouts/post-1.png',
                     '2'  => get_template_directory_uri().'/vc_elements/layouts/post-2.png',
                     '3'  => get_template_directory_uri().'/vc_elements/layouts/post-3.png',
+                    '4'  => get_template_directory_uri().'/vc_elements/layouts/post-4.jpg',
                 ),
                 'std'   => '1',
                 'group' => esc_html__('Layouts','theclick'),
