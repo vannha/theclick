16b1a3e131cff17b8ea97be889915202f4c36cc5
vannha <phongnhatk21@gmail.com>
2020-04-05 10:48:50 +0700

    commit -

--- vc_templates/ef5_banner.php
+++ vc_templates/ef5_banner.php
@@ -19,7 +19,9 @@ $el_id = !empty($el_id) ? 'ef5-banner-' . $el_id : uniqid('ef5-banner-');
             break;
         case '3': 
             $this->theclick_banner_main_media($atts,['class' => '']);
-            echo '<div class="gradient"></div><div class="content-wrap">';
+            var_dump(expression)
+            if(isset($show_overlay) && show_overlay echo '<div class="gradient"></div>';
+            echo '<div class="content-wrap">';
             $this->theclick_banner_main_title($atts,['class' => '']);
             $this->theclick_banner_sub_title($atts,['class' => '']);
             $this->theclick_banner_button($atts,['class' => '']);
