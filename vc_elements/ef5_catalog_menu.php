ccfaaf83b2406f108344dec1d90184bbb267c690
vannha <phongnhatk21@gmail.com>
2020-05-09 09:49:52 +0700

    commit -

--- vc_elements/ef5_catalog_menu.php
+++ vc_elements/ef5_catalog_menu.php
@@ -5,67 +5,63 @@ vc_map(array(
     'category'    => esc_html__('TheClick', 'theclick'),
     'description' => esc_html__('Add product category list with custom link', 'theclick'),
     'icon'        => '',
-    'params'      => array_merge(
+    'params'      => array(
         array(
-            array(
-                'type'       => 'img',
-                'heading'    => esc_html__('Layout Template','theclick'),
-                'param_name' => 'layout_template',
-                'value'      =>  array(
-                    '1'         => get_template_directory_uri().'/vc_elements/layouts/catalog-menu-1.jpg',
-                ),
-                'std'        => '1'
+            'type'       => 'img',
+            'heading'    => esc_html__('Layout Template','theclick'),
+            'param_name' => 'layout_template',
+            'value'      =>  array(
+                '1'         => get_template_directory_uri().'/vc_elements/layouts/catalog-menu-1.jpg',
             ),
-             
-            array(
-                'type'       => 'textfield',
-                'heading'    => esc_html__('Extra Class','theclick'),
-                'param_name' => 'el_class',
-                'value'      => '',
-                'description' => esc_html__('Style particular content element differently - add a class name and refer to it in custom CSS.', 'theclick'),
-            ),
-            /* Clients Settings */
-            array(
-                'type'          => 'textfield',
-                'heading'       => esc_html__('Upload image size','theclick'),
-                'description'   => esc_html__('Enter image size (Example: "thumbnail", "medium", "large", "post-thumbnail", "full". Alternatively enter size in pixels (Example: 200x100 (Width x Height)).','theclick'),
-                'param_name'    => 'thumbnail_size',
-                'value'         => '760',
-                'std'           => '530',
-            ),
-            array(
-                'type'       => 'param_group',
-                'heading'    => esc_html__( 'Add items', 'theclick' ),
-                'param_name' => 'values',
-                'param_name' => 'cl_group_1',
-                'value'      =>  urlencode( json_encode( array())),
-                'params'     => array(
-                    array(
-                        "type"          => "textfield",
-                        "heading"       => esc_html__("Title", 'theclick'),
-                        "param_name"    => "title_1",
-                        "value"         => '',
-                        'admin_label'   => true
-                    ),
-                    array(
-                        'type'        => 'attach_image',
-                        'heading'     => esc_html__( 'Category Image', 'theclick' ),
-                        'param_name'  => 'image',
-                        'admin_label' => true,
-                        'edit_field_class' => 'vc_col-sm-6'
-                    ),
-                    array(
-                        'type'        => 'vc_link',
-                        'heading'     => esc_html__( 'Link', 'theclick' ),
-                        'param_name'  => 'category_link'
-                    ),
-                ),
-                'group'     => 'Catalog'
-            ),
-             
+            'std'        => '1'
         ),
          
-    )
+        array(
+            'type'       => 'textfield',
+            'heading'    => esc_html__('Extra Class','theclick'),
+            'param_name' => 'el_class',
+            'value'      => '',
+            'description' => esc_html__('Style particular content element differently - add a class name and refer to it in custom CSS.', 'theclick'),
+        ),
+      
+        array(
+            'type'          => 'textfield',
+            'heading'       => esc_html__('Upload image size','theclick'),
+            'description'   => esc_html__('Enter image size (Example: "thumbnail", "medium", "large", "post-thumbnail", "full". Alternatively enter size in pixels (Example: 200x100 (Width x Height)).','theclick'),
+            'param_name'    => 'thumbnail_size',
+            'value'         => '760',
+            'std'           => '530',
+        ),
+        array(
+            'type'       => 'param_group',
+            'heading'    => esc_html__( 'Add items', 'theclick' ),
+            'param_name' => 'values',
+            'param_name' => 'cl_group_1',
+            'value'      =>  urlencode( json_encode( array())),
+            'params'     => array(
+                array(
+                    "type"          => "textfield",
+                    "heading"       => esc_html__("Title", 'theclick'),
+                    "param_name"    => "title_1",
+                    "value"         => '',
+                    'admin_label'   => true
+                ),
+                array(
+                    'type'        => 'attach_image',
+                    'heading'     => esc_html__( 'Category Image', 'theclick' ),
+                    'param_name'  => 'image',
+                    'admin_label' => true,
+                    'edit_field_class' => 'vc_col-sm-6'
+                ),
+                array(
+                    'type'        => 'vc_link',
+                    'heading'     => esc_html__( 'Link', 'theclick' ),
+                    'param_name'  => 'category_link'
+                ),
+            ),
+            'group'     => 'Catalog'
+        ),
+    )         
 ));
 
 class WPBakeryShortCode_ef5_catalog_menu extends WPBakeryShortCode
