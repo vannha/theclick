21213670b39e3b3a3f526e6e2a9862d41e05d1df
vannha <phongnhatk21@gmail.com>
2020-05-09 08:37:46 +0700

    commit -

--- template-parts/header/layout-9.php
+++ template-parts/header/layout-9.php
@@ -8,21 +8,17 @@
         <div class="<?php theclick_header_inner_class(); ?>">
             <div class="header-inner-wrap">
                 <div class="row justify-content-between align-items-center">
-                    <div class="col-auto">
-                        <div class="row">
-                            <div class="ef5-logo col-auto">
-                                <?php get_template_part('template-parts/header/header-logo'); ?>
-                            </div>
-                            <div class="ef5-navigation-wrap col-auto">
-                                <div class="d-flex align-items-center">
-                                    <?php
-                                    theclick_header_menu(['class' => 'ef5-navs']); 
-                                    ?>
-                                </div>
-                            </div>
+                    <div class="ef5-logo col-auto">
+                        <?php get_template_part('template-parts/header/header-logo'); ?>
+                    </div>
+                    <div class="ef5-navigation-wrap col-auto">
+                        <div class="d-flex align-items-center">
+                            <?php
+                            theclick_header_menu(['class' => 'ef5-navs']); 
+                            ?>
                         </div>
                     </div>
-                    <div class="header-attrs col-auto">
+                    <div class="header-attrs col">
                         <div class="header-attr-wrap nav-extra justify-content-end">
                             <?php
                             theclick_header_signin_signup(['class' => 'd-none d-md-block']);
