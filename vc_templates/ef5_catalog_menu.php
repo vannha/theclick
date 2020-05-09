4c6b5c70ffc368f94649e35c9f7785047f6be761
vannha <phongnhatk21@gmail.com>
2020-05-09 09:54:55 +0700

    commit -

--- vc_templates/ef5_catalog_menu.php
+++ vc_templates/ef5_catalog_menu.php
@@ -4,21 +4,11 @@ $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
 extract( $atts );
 
 $cl_group_1 = (array) vc_param_group_parse_atts( $cl_group_1 );
-if(empty($values[0])) {
-    echo '<p class="require required">'.esc_html__('Please add a client logo!','theclick').'</p>';
+if(empty($cl_group_1[0])) {
+    echo '<p class="require required">'.esc_html__('Please add a Category item!','theclick').'</p>';
     return;
 }
-$count = count($values);
-$i=1;
-$j=0;
-
-$owl_item_space = '';
-$dot_thumbnail_size = '50';
-$item_attrs = [];
-
-if(!empty($atts['margin']) && $atts['number_row'] > 1 ) {
-    $owl_item_space = 'style="margin-bottom:'.$margin.'px"';
-}
+ 
 if(count($cl_group_1) <=0 ) return;
 ?>
 <div class="<?php $this->theclick_catalog_menu_wrap_css_class($atts);?>">
