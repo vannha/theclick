0510917f5e9ab62f4c0fbbecdf2dfc1ed15a743d
vannha <phongnhatk21@gmail.com>
2020-05-12 08:51:19 +0700

    commit -

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -165,8 +165,8 @@
                 var data_parent = $(this).attr('data-parent');
                 $(this).parents('.ef5-catalog-menu').find('.catalog-child-wrap > :not(.'+data_parent+')').fadeTo('0.1',0).css('display','none');
                 $(this).parents('.ef5-catalog-menu').find('.catalog-img-wrap > :not(.'+data_parent+')').fadeTo('0.1',0).css('display','none');
-                $(this).parents('.ef5-catalog-menu').find('.catalog-child-wrap .'+data_parent).fadeTo('0.3',1).css('display','block');
-                $(this).parents('.ef5-catalog-menu').find('.catalog-img-wrap .'+data_parent).fadeTo('0.3',1).css('display','block');
+                $(this).parents('.ef5-catalog-menu').find('.catalog-child-wrap .'+data_parent).fadeTo('0.2',1).css('display','block');
+                $(this).parents('.ef5-catalog-menu').find('.catalog-img-wrap .'+data_parent).fadeTo('0.2',1).css('display','block');
             }); 
         } 
     }
