aff8d8f3f4b6df86fea3721a11fc7f9b0d7e5a2e
vannha <phongnhatk21@gmail.com>
2020-05-22 17:12:04 +0700

    commit -

--- inc/woo/_hook_loop.php
+++ inc/woo/_hook_loop.php
@@ -81,6 +81,8 @@ if(!function_exists('theclick_woocommerce_before_shop_loop')){
 add_filter( 'loop_shop_columns', 'theclick_loop_shop_columns', 20 );
 function theclick_loop_shop_columns() {
 	$columns = theclick_get_opts('products_columns', 4);  
+	if(isset($_GET['layout']) && ( $_GET['layout'] == 'left' || $_GET['layout'] == 'right'))
+		$columns = $columns - 1;
 	return $columns;
 }
 
