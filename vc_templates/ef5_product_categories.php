05f238574a3f5fb7b1bc3b71f9f97edbee79f012
vannha <phongnhatk21@gmail.com>
2020-05-23 10:25:27 +0700

    commit -

--- vc_templates/ef5_product_categories.php
+++ vc_templates/ef5_product_categories.php
@@ -6,10 +6,6 @@ if(!empty($taxonomies)){
 	$taxonomies_arr = explode(',', $taxonomies);
 	
 	foreach ($taxonomies_arr as $tax) {
-		$terms_args['slug'] = $tax;
-		$terms_args =  [
-	        'taxonomy' => 'product_cat'
-	    ];
 		$cat_obj = get_terms(['slug' => $tax, 'taxonomy' => 'product_cat']);
 		var_dump($cat_obj);
 	}
