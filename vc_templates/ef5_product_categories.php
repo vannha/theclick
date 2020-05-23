80b9229fbea657033e515d5ccfbdfd881d075c1e
vannha <phongnhatk21@gmail.com>
2020-05-23 10:59:22 +0700

    commit -

--- vc_templates/ef5_product_categories.php
+++ vc_templates/ef5_product_categories.php
@@ -6,14 +6,13 @@ if(!empty($taxonomies)){
 	$taxonomies_arr = explode(',', $taxonomies);
 	foreach ($taxonomies_arr as $tax) {
 		$cat = get_term_by('slug', $tax, 'product_cat');
-		
 		$thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true ); 
- 
 	    $image = wp_get_attachment_url( $thumbnail_id ); 
-	    var_dump($image);
+	    if($image){
+	    	echo '<img src="'.$image.'" alt="'.$cat->name.'" />';
+	    } 
 	}
-	//$terms = get_terms(['slug' => $taxonomies_arr, 'taxonomy' => 'product_cat']);
-	
+	 
 	/*foreach ($terms as $cat) {
 		var_dump($cat);
 		term_id.
