babbdb8b521f67b4d071d0b790525328838123e3
vannha <phongnhatk21@gmail.com>
2020-05-23 10:14:59 +0700

    commit -

--- vc_templates/ef5_product_categories.php
+++ vc_templates/ef5_product_categories.php
@@ -1,5 +1,13 @@
 <?php 
 $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
 extract( $atts );
-var_dump($taxonomies);
+ 
+if(!empty($taxonomies)){
+	$taxonomies_arr = explode(',', $taxonomies);
+	foreach ($taxonomies_arr as $tax) {
+
+		$cat_obj = get_terms($tax); 
+		var_dump($cat_obj);
+	}
+}
 ?>
