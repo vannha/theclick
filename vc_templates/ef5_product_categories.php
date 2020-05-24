72b1664bf222ea109798df5b73718cdb22d18ab8
vannha <phongnhatk21@gmail.com>
2020-05-24 08:38:48 +0700

    commit -

--- vc_templates/ef5_product_categories.php
+++ vc_templates/ef5_product_categories.php
@@ -13,7 +13,7 @@ if(!empty($taxonomies)){
 		$img_size = $img_sizes;
 		if( $i == 1 ){
 			$item_cls = 'col-10 offset-lg-1';
-			$img_size = '100x600';
+			$img_size = '1000x600';
 		}
 		if( $i == 2 || $i == 3 ){
 			$item_cls = 'col-6';
