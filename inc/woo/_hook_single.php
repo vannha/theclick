1d1b8a847150e63dc6597083bb44e18adbe535ee
vannha <phongnhatk21@gmail.com>
2020-05-28 15:25:29 +0700

    commit -

--- inc/woo/_hook_single.php
+++ inc/woo/_hook_single.php
@@ -271,7 +271,7 @@ add_filter('woocommerce_product_additional_information_heading', function(){ ret
 add_action( 'the_post', 'theclick_empty_related_array' );
 function theclick_empty_related_array($array){
 	$single_product_related = theclick_get_theme_opt('single_product_related','0');
-	var_dump($single_product_related);  
+	var_dump($single_product_related);  die('xxx');
 	if($single_product_related=='0')
 	$array = [];
 	return $array;
