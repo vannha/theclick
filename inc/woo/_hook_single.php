d532fb695d16ba028fae107c5137bf7e9ab91f88
vannha <phongnhatk21@gmail.com>
2020-05-28 14:44:04 +0700

    commit -

--- inc/woo/_hook_single.php
+++ inc/woo/_hook_single.php
@@ -282,7 +282,7 @@ if(!function_exists('theclick_woocommerce_output_related_products_args')){
 if(!function_exists('theclick_single_product_scripts')){
 	add_action('wp_enqueue_scripts', 'theclick_single_product_scripts', 0);
 	function theclick_single_product_scripts(){
-		if(is_singular('product')){
+		if(is_singular('product')){ die('aaa');
 			wp_enqueue_script('owl-carousel');
 			wp_enqueue_style('owl-carousel');
 		}
