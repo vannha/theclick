ffd3d56dd2b88abaeb9e4ba354e89ac3afdd557d
vannha <phongnhatk21@gmail.com>
2020-06-09 14:06:34 +0700

    commit -

--- inc/woo/_hook_single.php
+++ inc/woo/_hook_single.php
@@ -157,7 +157,7 @@ if(!function_exists('theclick_product_gallery_thumbnail_sync')){
         ]);
         $post_thumbnail_id = $product->get_image_id();
     	$attachment_ids = array_merge( (array)$post_thumbnail_id , $product->get_gallery_image_ids() );
-var_dump($attachment_ids);
+ 
         if('simple' === $args['gallery_layout'] || empty($attachment_ids[0])) return;
         $flex_class = '';
 
@@ -186,9 +186,10 @@ var_dump($attachment_ids);
 
 	    }
 	    $gallery_css_class = ['wc-gallery-sync', $gallery_layout, $product_gallery_thumb_position];
+	    $gal_cls = $product_style == 'sticky' ? 'wc-gallery-slicky' : 'wc-gallery-sync-slides flexslider '.$flex_class;
     ?>
     	<div class="<?php echo trim(implode(' ', $gallery_css_class));?>" data-thumb-w="<?php echo esc_attr($thumb_w);?>" data-thumb-h="<?php echo esc_attr($thumb_h);?>" data-thumb-margin="<?php echo esc_attr($thumb_margin); ?>">
-			<div class="wc-gallery-sync-slides flexslider <?php echo esc_attr($flex_class);?>">
+			<div class="<?php echo esc_attr($gal_cls);?>">
 	            <?php foreach ( $attachment_ids as $attachment_id ) { ?>
 	                <div class="wc-gallery-sync-slide flex-control-thumb"><?php theclick_image_by_size(['id' => $attachment_id, 'size' => $thumbnail_size]);?></div>
 	            <?php } ?>
