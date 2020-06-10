d0ecd83e5a0994ddf24a646eff6589b911fc650a
vannha <phongnhatk21@gmail.com>
2020-06-10 10:36:06 +0700

    commit -

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1076,7 +1076,7 @@
                 $( '.product-style-sticky .wc-gallery-sticky .flex-control-thumb.idx-'+idx).addClass('active');
             }
         });
-    }
+    });
      
 
     function theclick_woo_filter_sidebar(){
