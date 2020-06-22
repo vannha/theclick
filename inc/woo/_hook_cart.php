0096432bdba34046526875df78faaa6ec4a0c88b
vannha <phongnhatk21@gmail.com>
2020-06-22 09:46:05 +0700

    commit -

--- inc/woo/_hook_cart.php
+++ inc/woo/_hook_cart.php
@@ -7,7 +7,16 @@ if(!function_exists('theclick_woocommerce_cart_item_thumbnail')){
 	function  theclick_woocommerce_cart_item_thumbnail($thumbnail, $cart_item, $cart_item_key){
 		$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 		$thumbnail_id = get_post_thumbnail_id($product_id);
-		$thumbnail = theclick_image_by_size(['id' => $thumbnail_id, 'size' => apply_filters('theclick_woocommerce_cart_item_thumbnail_size', '100x100'),'echo'   => false]);
+		// thumbnail size
+		$thumbnail_size_w = str_replace('px', '', theclick_configs('theclick_woocommerce_cart_item_thumbnail_size')['width']);
+		$thumbnail_size_h = str_replace('px', '', theclick_configs('theclick_woocommerce_cart_item_thumbnail_size')['height']);
+		$thumbnail_size = implode('x',[$thumbnail_size_w, $thumbnail_size_h]);
+		
+		$thumbnail = theclick_image_by_size([
+			'id' => $thumbnail_id, 
+			'size' => $thumbnail_size,
+			'echo' => false
+		]);
 		return $thumbnail;
 	}
 }
@@ -27,7 +36,7 @@ if(!function_exists('theclick_woocommerce_cart_item_name')){
 		$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
 
 		$terms = get_the_terms($product_id, 'pa_brand');
-        if( !empty($terms) && !is_wp_error($terms)){
+        if(!is_wp_error($terms)){
             $count = count($terms);
         } else {
             $count = 0;
@@ -58,27 +67,43 @@ if(!function_exists('theclick_woocommerce_cart_item_name')){
  * Custom Cart Actions Layout
 */
 if(!function_exists('theclick_woocommerce_cart_actions')){
-	add_filter('woocommerce_cart_actions','theclick_woocommerce_cart_actions', 10);
+	add_action('woocommerce_cart_actions','theclick_woocommerce_cart_actions', 10);
 	remove_action('woocommerce_cart_collaterals','woocommerce_cart_totals',10);
 	function theclick_woocommerce_cart_actions(){
 		?>
-		<div class="ef5-cart-actions-wrap row">
-			<div class="col-lg-6">
-				<?php if ( wc_coupons_enabled() ) { ?>
-					<div class="ef5-cart-coupon coupon">
-						<input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'theclick' ); ?>" />
-						<button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'theclick' ); ?>"><?php esc_attr_e( 'Apply coupon', 'theclick' ); ?></button>
-						<?php do_action( 'woocommerce_cart_coupon' ); ?>
-					</div>
-				<?php } ?>
-			</div>
-			<div class="ef5-cart-totals col-lg-6">
-				<?php woocommerce_cart_totals(); ?>
-			</div>
+		<div class="ef5-cart-actions-wrap row justify-content-between">
+			<div class="col-12 col-lg-auto empty-none"><div class="ef5-cart-action-col ef5-cart-action-left d-flex gutters-30 justify-content-center justify-content-lg-start"><?php do_action('theclick_woocommerce_cart_actions_left'); ?></div></div>
+			<div class="col-12 col-lg-auto empty-none"><div class="ef5-cart-action-col ef5-cart-action-right d-flex gutters-10 justify-content-center justify-content-lg-end"><?php do_action('theclick_woocommerce_cart_actions_right'); ?></div></div>
 		</div>
-		
 		<?php
 	}
+}
+/* Return Shop Button */
+if(!function_exists('theclick_woocommerce_return_shop')){
+	add_action('theclick_woocommerce_cart_actions_left', 'theclick_woocommerce_return_shop', 1);
+	function theclick_woocommerce_return_shop(){
+		if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
+			<div class="ef5-return-shop">
+				<a class="ef5-btn accent fill ef5-btn-xmd" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
+					<span class="ef5-return-shop-icon"></span>
+					<span><?php esc_html_e( 'Continue Shopping', 'theclick' ); ?></span>
+				</a>
+			</div>
+		<?php endif; 
+	}
+}
+// Coupon
+add_action('theclick_woocommerce_cart_actions_left', 'theclick_woocommerce_cart_coupon', 2);
+function theclick_woocommerce_cart_coupon(){
+	if ( wc_coupons_enabled() ) { 
+	?>
+	<div class="ef5-cart-coupon coupon d-flex justify-content-center">
+		<input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Enter your coupon', 'theclick' ); ?>" />
+		<button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Confirm', 'theclick' ); ?>"><?php esc_attr_e( 'Confirm', 'theclick' ); ?></button>
+		<?php do_action( 'woocommerce_cart_coupon' ); ?>
+	</div>
+	<?php 
+	} 
 }
 
 /**
@@ -91,7 +116,7 @@ if(!function_exists('theclick_woocommerce_cart_actions')){
  *
 */
 if(!function_exists('theclick_woocommerce_after_cart_table')){
-	add_action('woocommerce_after_cart_table','theclick_woocommerce_after_cart_table', 1);
+	//add_action('woocommerce_after_cart_table','theclick_woocommerce_after_cart_table', 1);
 	/**
 	 * Change proceed to checkout button position
 	*/
@@ -104,26 +129,12 @@ if(!function_exists('theclick_woocommerce_after_cart_table')){
 			<?php do_action('theclick_woocommerce_after_cart_table_left'); ?>
 		</div>
 		<div class="col-lg-6 col-right align-items-center d-md-flex justify-content-lg-end justify-content-center">
-			<button type="submit" class="btn button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'theclick' ); ?>"><span class="fa fa-refresh">&nbsp;&nbsp;</span><span><?php esc_html_e( 'Update cart', 'theclick' ); ?></span></button>
-			<a href="<?php echo esc_url( wc_get_checkout_url() );?>" class="checkout-button ef5-btn accent outline ef5-btn-lg">
-				<span class="far fa-plus">&nbsp;&nbsp;</span><span><?php esc_html_e( 'Order Now', 'theclick' ); ?></span>
-			</a>
 		</div>
 	</div>
 	<?php
 	}
 }
-/* Return Shop Button */
-if(!function_exists('theclick_woocommerce_return_shop')){
-	add_action('theclick_woocommerce_after_cart_table_left', 'theclick_woocommerce_return_shop');
-	function theclick_woocommerce_return_shop(){
-		if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
-			<a class="ef5-btn accent fill ef5-btn-lg" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-				<span class="far fa-shopping-basket">&nbsp;&nbsp;</span><span><?php esc_html_e( 'Continue Shopping', 'theclick' ); ?></a>
-			</a>
-		<?php endif; 
-	}
-}
+
 
 // check for empty-cart get param to clear the cart
 if(!function_exists('theclick_woocommerce_clear_cart_url')){
@@ -135,12 +146,56 @@ if(!function_exists('theclick_woocommerce_clear_cart_url')){
 	}
 }
 if(!function_exists('theclick_woocommerce_clear_cart_button')){
-	add_action('theclick_woocommerce_after_cart_table_left', 'theclick_woocommerce_clear_cart_button');
+	add_action('theclick_woocommerce_cart_actions_right', 'theclick_woocommerce_clear_cart_button', 1);
 	function theclick_woocommerce_clear_cart_button(){
 		$link = wc_get_cart_url();
         $link = add_query_arg( 'empty-cart', '', $link );
 		?>
-			<a class="ef5-btn red outline ef5-btn-lg" href="<?php echo esc_url($link);?>"><span class="far fa-times">&nbsp;&nbsp;</span><span><?php esc_html_e( 'Clear Shopping Cart', 'theclick' ); ?></span></a>
+			<div class="ef5-clear-cart">
+				<a class="ef5-btn accent outline2 ef5-btn-xmd minw-180" href="<?php echo esc_url($link);?>"><span class="ef5-clear-cart-icon"></span><span><?php esc_html_e( 'Clear Cart', 'theclick' ); ?></span></a>
+			</div>
+		<?php
+	}
+}
+
+// Update Cart 
+if(!function_exists('theclick_woocommerce_cart_update')){
+	add_action('theclick_woocommerce_cart_actions_right','theclick_woocommerce_cart_update', 2);
+	function theclick_woocommerce_cart_update(){
+		?>
+		<div class="ef5-update-cart">
+			<button type="submit" class="ef5-btn ef5-btn-xmd accent fill minw-180" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'theclick' ); ?>"><span class="ef5-update-cart-icon"></span><span><?php esc_html_e( 'Update cart', 'theclick' ); ?></span></button>
+		</div>
+		<?php
+	}
+}
+
+// Cart Totals
+if(!function_exists('theclick_woocommerce_cart_collaterals')){
+	remove_action('woocommerce_cart_collaterals','woocommerce_cart_totals',10);
+	add_action('woocommerce_cart_collaterals', 'theclick_woocommerce_cart_collaterals');
+	add_action('theclick_woocommerce_cart_collaterals', 'woocommerce_cart_totals', 1);
+	function theclick_woocommerce_cart_collaterals(){
+		?>
+		<div class="row justify-content-end">
+			<div class="col-12 col-md-auto">
+				<?php do_action('theclick_woocommerce_cart_collaterals'); ?>
+			</div>
+		</div>
+		<?php
+	}
+}
+
+// Checkout button
+if(!function_exists('theclick_woocommerce_cart_checkout_button')){
+	add_action('theclick_woocommerce_cart_collaterals','theclick_woocommerce_cart_checkout_button', 2);
+	function theclick_woocommerce_cart_checkout_button(){
+		?>
+		<div class="ef5-cart-checkout-btn">
+			<a href="<?php echo esc_url( wc_get_checkout_url() );?>" class="ef5-btn accent fill w-100 ef5-btn-xmd">
+					<span class="ef5-cart-checkout-icon"></span><span><?php esc_html_e( 'Checkout', 'theclick' ); ?></span>
+				</a>
+		</div>
 		<?php
 	}
 }
