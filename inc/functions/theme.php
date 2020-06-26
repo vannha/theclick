cfa1adc7bd18c60fe212c5763b5030169b8b0cee
vannha <phongnhatk21@gmail.com>
2020-06-26 09:53:10 +0700

    commit -

--- inc/functions/theme.php
+++ inc/functions/theme.php
@@ -154,7 +154,6 @@ function theclick_sidebar_position(){
     } elseif(is_page()){
         if (class_exists('WooCommerce') && (is_checkout() || is_cart())) {
             $sidebar_position = theclick_get_opts('page_sidebar_pos',theclick_shop_sidebar_position());
-            $sidebar_position = theclick_get_opts('page_sidebar_pos','none');
             var_dump($sidebar_position);
         } else {
             $sidebar_position = theclick_get_opts('page_sidebar_pos',theclick_page_sidebar_position());
